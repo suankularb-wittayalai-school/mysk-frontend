e58af00d09ebd8eec802e7ce6a1f911933532678
SiravitPhokeed <siravitsasa@gmail.com>
2022-03-15 13:34:44 +0700

    feat: “No relevant news” message

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -279,92 +279,100 @@ const NewsSection = (): JSX.Element => {
         onChange={(newFilter: Array<string>) => setNewsFilter(newFilter)}
         scrollable={true}
       />
-      <XScrollContent>
-        {filteredNews.map((newsItem) => (
-          <li key={`${newsItem.type}-${newsItem.id}`}>
-            <Card type="stacked" appearance="outlined">
-              <CardHeader
-                icon={
-                  newsItem.type == "form" ? (
-                    <MaterialIcon icon="edit" />
-                  ) : newsItem.type == "payment" ? (
-                    <MaterialIcon icon="account_balance" />
-                  ) : (
-                    <MaterialIcon icon="information" />
-                  )
-                }
-                title={
-                  <h3 className="text-lg font-medium">
-                    {newsItem.content[locale].title}
-                  </h3>
-                }
-                label={
-                  <div className="flex divide-x divide-outline">
-                    <span className="pr-2">
-                      {t(`news.itemType.${newsItem.type}`)}
-                    </span>
-                    <time className="pl-2 text-outline">
-                      {newsItem.postDate.toLocaleDateString(locale, {
-                        year: isThisYear(newsItem.postDate)
-                          ? undefined
-                          : "numeric",
-                        month: "short",
-                        day: "numeric",
-                      })}
-                    </time>
-                  </div>
-                }
-                end={
-                  newsItem.type != "news" ? (
-                    <div
-                      className={`${
-                        newsItem.done
-                          ? "container-primary"
-                          : (newsItem.type == "form" ||
-                              newsItem.type == "payment") &&
-                            newsItem.dueDate &&
-                            isPast(newsItem.dueDate)
-                          ? "bg-error text-on-error"
-                          : "container-tertiary"
-                      } grid aspect-square w-10 place-content-center rounded-xl`}
-                    >
-                      {newsItem.done ? (
-                        <MaterialIcon icon="done" />
-                      ) : (
-                        <MaterialIcon icon="close" />
-                      )}
-                    </div>
-                  ) : undefined
-                }
-                className="font-display"
-              />
-              <CardSupportingText>
-                <p className="max-lines-2">
-                  {
-                    newsItem.content[locale == "en-US" ? "en-US" : "th"]
-                      .supportingText
-                  }
-                </p>
-              </CardSupportingText>
-              <CardActions>
-                <LinkButton
-                  name={t(
-                    `news.itemAction.${newsItem.type}${
-                      newsItem.type != "news"
-                        ? `.${newsItem.done ? "edit" : "do"}`
-                        : ""
-                    }`
-                  )}
-                  type="filled"
-                  url={`/${newsItem.type}/${newsItem.id}`}
-                  LinkElement={Link}
-                  className="container-secondary"
-                />
-              </CardActions>
-            </Card>
+      {filteredNews.length == 0 ? (
+        <ul className="px-4">
+          <li className="bg-surface-1 grid h-[13.75rem] place-content-center rounded-xl text-center text-on-surface">
+            {t("news.noRelevantNews")}
           </li>
-        ))}
-      </XScrollContent>
+        </ul>
+      ) : (
+        <XScrollContent>
+          {filteredNews.map((newsItem) => (
+            <li key={`${newsItem.type}-${newsItem.id}`}>
+              <Card type="stacked" appearance="outlined">
+                <CardHeader
+                  icon={
+                    newsItem.type == "form" ? (
+                      <MaterialIcon icon="edit" />
+                    ) : newsItem.type == "payment" ? (
+                      <MaterialIcon icon="account_balance" />
+                    ) : (
+                      <MaterialIcon icon="information" />
+                    )
+                  }
+                  title={
+                    <h3 className="text-lg font-medium">
+                      {newsItem.content[locale].title}
+                    </h3>
+                  }
+                  label={
+                    <div className="flex divide-x divide-outline">
+                      <span className="pr-2">
+                        {t(`news.itemType.${newsItem.type}`)}
+                      </span>
+                      <time className="pl-2 text-outline">
+                        {newsItem.postDate.toLocaleDateString(locale, {
+                          year: isThisYear(newsItem.postDate)
+                            ? undefined
+                            : "numeric",
+                          month: "short",
+                          day: "numeric",
+                        })}
+                      </time>
+                    </div>
+                  }
+                  end={
+                    newsItem.type != "news" ? (
+                      <div
+                        className={`${
+                          newsItem.done
+                            ? "container-primary"
+                            : (newsItem.type == "form" ||
+                                newsItem.type == "payment") &&
+                              newsItem.dueDate &&
+                              isPast(newsItem.dueDate)
+                            ? "bg-error text-on-error"
+                            : "container-tertiary"
+                        } grid aspect-square w-10 place-content-center rounded-xl`}
+                      >
+                        {newsItem.done ? (
+                          <MaterialIcon icon="done" />
+                        ) : (
+                          <MaterialIcon icon="close" />
+                        )}
+                      </div>
+                    ) : undefined
+                  }
+                  className="font-display"
+                />
+                <CardSupportingText>
+                  <p className="max-lines-2">
+                    {
+                      newsItem.content[locale == "en-US" ? "en-US" : "th"]
+                        .supportingText
+                    }
+                  </p>
+                </CardSupportingText>
+                <CardActions>
+                  <LinkButton
+                    name={t(
+                      `news.itemAction.${newsItem.type}${
+                        newsItem.type != "news"
+                          ? `.${newsItem.done ? "edit" : "do"}`
+                          : ""
+                      }`
+                    )}
+                    type="filled"
+                    url={`/${newsItem.type}/${newsItem.id}`}
+                    LinkElement={Link}
+                    className="container-secondary"
+                  />
+                </CardActions>
+              </Card>
+            </li>
+          ))}
+        </XScrollContent>
+      )}
       <div className="flex flex-row items-center justify-end gap-2">
         <LinkButton
           name={t("news.action.seeAll")}
