9a46644423219cf20038e2c3042e0b968c065bd7
SiravitPhokeed <siravitsasa@gmail.com>
2022-03-21 00:48:26 +0700

    feat: Change Pwd dialog has Discard Draft warning

--- src/components/dialogs/ChangePassword.tsx
+++ src/components/dialogs/ChangePassword.tsx
@@ -1,13 +1,16 @@
 // Modules
 import { useTranslation } from "next-i18next";
+import { useState } from "react";
 
 // SK Components
 import {
   Dialog,
   DialogSection,
   KeyboardInput,
-  MaterialIcon,
 } from "@suankularb-components/react";
+
+// Components
+import DiscardDraft from "@components/dialogs/DiscardDraft";
 
 // Types
 import { DialogProps } from "@utils/types/common";
@@ -17,32 +20,34 @@ import { changePassword } from "@utils/backend/account";
 
 const ChangePassword = ({ show, onClose }: DialogProps): JSX.Element => {
   const { t } = useTranslation("account");
+  const [showDiscard, setShowDiscard] = useState<boolean>(false);
 
   return (
-    <Dialog
-      type="regular"
-      label="change-password"
-      title={t("dialog.changePassword.title")}
-      supportingText={t("dialog.changePassword.supportingText")}
-      actions={[
-        { name: t("dialog.changePassword.action.cancel"), type: "close" },
-        { name: t("dialog.changePassword.action.save"), type: "submit" },
-      ]}
-      show={show}
-      onClose={() => onClose()}
-      onSubmit={() => {
-        changePassword();
-        onClose();
-      }}
-    >
-      {/* FIXME: Once `name` is no longer necessary, remove it */}
-      <DialogSection name="">
-        <KeyboardInput
-          name="original-password"
-          type="password"
-          label={t("dialog.changePassword.originalPwd")}
-          onChange={() => {}}
-        />
+    <>
+      <Dialog
+        type="regular"
+        label="change-password"
+        title={t("dialog.changePassword.title")}
+        supportingText={t("dialog.changePassword.supportingText")}
+        actions={[
+          { name: t("dialog.changePassword.action.cancel"), type: "close" },
+          { name: t("dialog.changePassword.action.save"), type: "submit" },
+        ]}
+        show={show}
+        onClose={() => setShowDiscard(true)}
+        onSubmit={() => {
+          changePassword();
+          onClose();
+        }}
+      >
+        {/* FIXME: Once `name` is no longer necessary, remove it */}
+        <DialogSection name="">
+          <KeyboardInput
+            name="original-password"
+            type="password"
+            label={t("dialog.changePassword.originalPwd")}
+            onChange={() => {}}
+          />
           <KeyboardInput
             name="new-password"
             type="password"
@@ -55,8 +60,17 @@ const ChangePassword = ({ show, onClose }: DialogProps): JSX.Element => {
             label={t("dialog.changePassword.confirmNewPwd")}
             onChange={() => {}}
           />
-      </DialogSection>
-    </Dialog>
+        </DialogSection>
+      </Dialog>
+      <DiscardDraft
+        show={showDiscard}
+        onClose={() => setShowDiscard(false)}
+        onSubmit={() => {
+          setShowDiscard(false);
+          onClose();
+        }}
+      />
+    </>
   );
 };
 
