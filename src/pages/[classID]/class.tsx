de9af1579b71d3eec66a3676be780a5ccf8369ce
SiravitPhokeed <siravitsasa@gmail.com>
2022-03-24 00:58:47 +0700

    feat: Student Class translations

--- src/pages/[classID]/class.tsx
+++ src/pages/[classID]/class.tsx
@@ -2,11 +2,14 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 // SK Components
 import {
+  Header,
   MaterialIcon,
   RegularLayout,
+  Section,
   Title,
 } from "@suankularb-components/react";
 
@@ -25,9 +28,34 @@ const Class: NextPage = () => {
         />
       }
     >
-      <p>TODO</p>
+      <Section labelledBy="class-counselors">
+        <Header
+          icon={<MaterialIcon icon="group" />}
+          text={t("classCounselors.title")}
+        />
+      </Section>
+      <Section labelledBy="class-contacts">
+        <Header
+          icon={<MaterialIcon icon="group" />}
+          text={t("classContacts.title")}
+        />
+      </Section>
+      <Section labelledBy="student-list">
+        <div className="layout-grid-cols-3--header">
+          <Header
+            icon={<MaterialIcon icon="group" />}
+            text={t("studentList.title")}
+          />
+        </div>
+      </Section>
     </RegularLayout>
   );
 };
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["common", "class"])),
+  },
+});
 
 export default Class;
