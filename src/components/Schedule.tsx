cd708ab82a7ea614f5bde26b0fad01bcc6a777d1
SiravitPhokeed <siravitsasa@gmail.com>
2022-04-01 04:09:28 +0700

    feat: Current Period Indicator

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,5 +1,5 @@
 // Modules
-import { setDay } from "date-fns";
+import { intervalToDuration, isWithinInterval, setDay } from "date-fns";
 import { AnimatePresence, motion } from "framer-motion";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
@@ -13,6 +13,7 @@ import {
 
 // Animations
 import { animationTransition } from "@utils/animations/config";
+import { periodTimes } from "@utils/helpers/schedule";
 
 const ScheduleDay = ({ day }: { day: ScheduleRowType["day"] }): JSX.Element => {
   const locale = useRouter().locale;
@@ -43,6 +44,9 @@ const ScheduleRow = ({
   const locale = useRouter().locale == "th" ? "th" : "en-US";
   const { t } = useTranslation("common");
 
+  const day = setDay(new Date(), scheduleRow.day);
+  const now = new Date();
+
   const periodWidth = 112;
 
   return (
@@ -62,7 +66,38 @@ const ScheduleRow = ({
             transition={animationTransition}
           >
             {schedulePeriod.subject ? (
-              <div className="container-secondary flex h-[3.75rem] flex-col rounded-lg px-4 py-2 leading-snug">
+              <div
+                className={`flex h-[3.75rem] flex-col rounded-lg px-4 py-2 leading-snug ${
+                  isWithinInterval(now, {
+                    start: new Date(
+                      day.setHours(
+                        periodTimes[schedulePeriod.periodStart].hours,
+                        periodTimes[schedulePeriod.periodStart].min,
+                        0,
+                        0
+                      )
+                    ),
+                    end: new Date(
+                      day.setHours(
+                        periodTimes[
+                          schedulePeriod.periodStart +
+                            schedulePeriod.duration -
+                            1
+                        ].hours,
+                        periodTimes[
+                          schedulePeriod.periodStart +
+                            schedulePeriod.duration -
+                            1
+                        ].min,
+                        0,
+                        0
+                      )
+                    ),
+                  })
+                    ? "container-tertiary shadow"
+                    : "container-secondary"
+                }`}
+              >
                 <span
                   className="max-lines-1 font-display text-xl font-medium"
                   title={schedulePeriod.subject.name[locale].name}
@@ -120,7 +155,11 @@ const Schedule = ({
         <ScheduleDay key={scheduleRow.day} day={scheduleRow.day} />
       ))}
     </div>
-    <div className={noScroll ? "grow" : "scroll-w-0 scroll-desktop grow sm:overflow-x-auto"}>
+    <div
+      className={
+        noScroll ? "grow" : "scroll-w-0 scroll-desktop grow sm:overflow-x-auto"
+      }
+    >
       <AnimatePresence initial={false}>
         <ul className="flex flex-col gap-2 py-1 pl-1 pr-4 sm:pr-0">
           {schedule.content.map((scheduleRow) => (
