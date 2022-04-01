69c73abdb4fd3ffcb92495feddd335e5f12c952e
SiravitPhokeed <siravitsasa@gmail.com>
2022-04-01 17:31:16 +0700

    feat: Dummy Subject for Subject Details page

--- src/pages/subjects/[subjectID].tsx
+++ src/pages/subjects/[subjectID].tsx
@@ -1,5 +1,7 @@
 // Modules
 import { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
+
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
@@ -11,14 +13,21 @@ import {
   Title,
 } from "@suankularb-components/react";
 
-const SubjectDetails: NextPage = () => {
+// Types
+import { Subject } from "@utils/types/subject";
+
+const SubjectDetails: NextPage<{ subject: Subject }> = ({ subject }) => {
   const { t } = useTranslation("subjects");
+  const locale = useRouter().locale == "en-US" ? "en-US" : "th";
 
   return (
     <RegularLayout
       Title={
         <Title
-          name={{ title: t("teaching.title") }}
+          name={{
+            title: subject.name[locale].name,
+            subtitle: subject.code[locale],
+          }}
           pageIcon={<MaterialIcon icon="school" />}
           backGoesTo="/home"
         />
@@ -33,12 +42,29 @@ export const getServerSideProps: GetServerSideProps = async ({
   locale,
   params,
 }) => {
+  const subject: Subject = {
+    id: 26,
+    code: { "en-US": "ENG32102", th: "อ32102" },
+    name: {
+      "en-US": { name: "English 4" },
+      th: { name: "ภาษาอังกฤษ 4" },
+    },
+    teachers: [],
+    subjectSubgroup: {
+      name: { "en-US": "English", th: "ภาษาอังกฤษ" },
+      subjectGroup: {
+        name: { "en-US": "Foreign Language", th: "ภาษาต่างประเทศ" },
+      },
+    },
+  };
+
   return {
     props: {
       ...(await serverSideTranslations(locale as string, [
         "common",
         "subjects",
       ])),
+      subject,
     },
   };
 };
