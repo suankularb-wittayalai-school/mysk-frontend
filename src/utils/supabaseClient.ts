11fa0aefc3078fded1de01c72ce5f59cd192c18c
Jimmy_Tempest <smartwattana@gmail.com>
2022-04-29 21:40:10 +0700

    feat: add supabase client file

---
+++ src/utils/supabaseClient.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js'
+
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing supabase config')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
