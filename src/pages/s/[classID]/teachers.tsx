ab85ea96d7577a743ca3010bf7ed4a85a736b127
IHasDiabetes <sadudee.the@student.sk.ac.th>
2022-05-02 17:07:46 +0700

    feat: ListItem + changed dummybase to Teacher

--- src/pages/s/[classID]/teachers.tsx
+++ src/pages/s/[classID]/teachers.tsx
@@ -31,11 +31,71 @@ const Teachers: NextPage = (): JSX.Element => {
       content: [
         {
           id: 1,
-          content: { name: "กฤชพล บุญพูลมี" },
+          content: {
+            id: 1,
+            prefix: "missus",
+            name: {
+              "en-US": { firstName: "Krichapon", lastName: "Boonpoonmee" },
+              th: { firstName: "กฤชพล", lastName: "บุญพูลมี" },
+            },
+            profile: "/images/dummybase/mattana.webp",
+            role: "teacher",
+            subjectsInCharge: [
+              {
+                id: 1,
+                code: {
+                  "en-US": "MATH6969",
+                  th: "ค6969",
+                },
+                name: {
+                  "en-US": {
+                    name: "Core Mathematics",
+                  },
+                  th: { name: "คณิตศาสตร์พื้นๆ" },
+                },
+                subjectSubgroup: {
+                  name: { "en-US": "Mathematics", th: "คณิตศาสตร์" },
+                  subjectGroup: {
+                    name: { "en-US": "Mathematics", th: "คณิตศาสตร์" },
+                  },
+                },
+              },
+            ],
+          },
         },
         {
           id: 2,
-          content: { name: "กฤษฎา อัศวสกุลเกียรติ" },
+          content: {
+            id: 2,
+            prefix: "miss",
+            name: {
+              "en-US": { firstName: "Krissada", lastName: "Krissy" },
+              th: { firstName: "กฤษฎา", lastName: "อัศวสกุลเกียรติ" },
+            },
+            profile: "/images/dummybase/taradol.webp",
+            role: "teacher",
+            subjectsInCharge: [
+              {
+                id: 2,
+                code: {
+                  "en-US": "MATH69420",
+                  th: "ค69420",
+                },
+                name: {
+                  "en-US": {
+                    name: "Additional Mathematics",
+                  },
+                  th: { name: "คณิตศาสตร์เพิ่มเติม" },
+                },
+                subjectSubgroup: {
+                  name: { "en-US": "Mathematics", th: "คณิตศาสตร์" },
+                  subjectGroup: {
+                    name: { "en-US": "Mathematics", th: "คณิตศาสตร์" },
+                  },
+                },
+              },
+            ],
+          },
         },
       ],
     },
@@ -44,7 +104,37 @@ const Teachers: NextPage = (): JSX.Element => {
       content: [
         {
           id: 3,
-          content: { name: "ธนกร อรรจนาวัฒน์" },
+          content: {
+            id: 3,
+            prefix: "master",
+            name: {
+              "en-US": { firstName: "Thanakorn", lastName: "Arjanawut" },
+              th: { firstName: "ธนกร", lastName: "อรรจนาวัฒน์" },
+            },
+            profile: "/images/dummybase/thanakorn.webp",
+            role: "teacher",
+            subjectsInCharge: [
+              {
+                id: 3,
+                code: {
+                  "en-US": "SCI6969",
+                  th: "ว6969",
+                },
+                name: {
+                  "en-US": {
+                    name: "Core Science",
+                  },
+                  th: { name: "วิทยาศาสตร์พื้นๆ" },
+                },
+                subjectSubgroup: {
+                  name: { "en-US": "Science", th: "วิทยาศาสตร์" },
+                  subjectGroup: {
+                    name: { "en-US": "Science", th: "วิทยาศาสตร์" },
+                  },
+                },
+              },
+            ],
+          },
         },
       ],
     },
@@ -73,17 +163,21 @@ const Teachers: NextPage = (): JSX.Element => {
                   // setPolicy(Parties[0].policy);
                   // setMainType("policy");
                 }}
+                className="!w-full"
               >
-                <Card
+                {/* <Card
                   type="horizontal"
                   className={className}
                   appearance="tonal"
                 >
                   <CardHeader title={content.name} />
-                </Card>
-                {/* <TeacherCard 
-                  teacher={}
-                /> */}
+                </Card> */}
+                <TeacherCard
+                  key={content.id}
+                  teacher={content}
+                  hasSubjectSubgroup
+                  className={className}
+                />
               </button>
             );
           }}
