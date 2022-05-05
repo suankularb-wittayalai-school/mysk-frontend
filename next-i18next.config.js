4496ddbc89fcce683e081033c61ac51f4f11747c
Siravit Phokeed <siravitsasa@gmail.com>
2022-05-05 21:20:52 +0700

    fix: Vercel i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "th",
     locales: ["en-US", "th"],
+    localePath: path.resolve("./public/static/locales"),
   },
 };
