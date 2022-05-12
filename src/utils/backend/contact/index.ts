1aa0ff8fee99ed2afc699ef147d4aa6916947f76
Jimmy_Tempest <smartwattana@gmail.com>
2022-05-12 17:20:28 +0700

    feat: class can now be updated with  minor bugs

--- src/utils/backend/contact/index.ts
+++ src/utils/backend/contact/index.ts
@@ -23,3 +23,25 @@ export async function createContact(
   }
   return { data: createdContact, error: null };
 }
+
+export async function updateContact(
+  contact: Contact
+): Promise<{ data: ContactDB[] | null; error: PostgrestError | null }> {
+  const { data: updatedContact, error: contactUpdateError } = await supabase
+    .from<ContactDB>("contact")
+    .update({
+      type: contact.type,
+      value: contact.value,
+      name_en: contact.name["en-US"],
+      name_th: contact.name.th,
+      include_students: contact.includes?.students,
+      include_parents: contact.includes?.parents,
+      include_teachers: contact.includes?.teachers,
+    })
+    .match({ id: contact.id });
+  if (contactUpdateError || !updatedContact) {
+    console.error(contactUpdateError);
+    return { data: null, error: contactUpdateError };
+  }
+  return { data: updatedContact, error: null };
+}
