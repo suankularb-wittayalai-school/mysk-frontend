9586675e254b756473796618112e1a16b07fccda
Siravit Phokeed <siravitsasa@gmail.com>
2022-06-22 23:57:33 +0700

    feat: Extend Period Indicator

--- src/components/schedule/SchedulePeriod.tsx
+++ src/components/schedule/SchedulePeriod.tsx
@@ -2,7 +2,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // SK Components
 import { MaterialIcon } from "@suankularb-components/react";
@@ -147,16 +147,13 @@ const SubjectSchedulePeriod = ({
       onBlur={() => setShowMenu(false)}
     >
       {role == "teacher" && (
-        <AnimatePresence>
-          {showMenu && (
-            <PeriodHoverMenu
-              day={day}
-              schedulePeriod={schedulePeriod}
-              setEditPeriod={setEditPeriod}
-              setDeletePeriod={setDeletePeriod}
-            />
-          )}
-        </AnimatePresence>
+        <PeriodHoverMenu
+          show={showMenu}
+          day={day}
+          schedulePeriod={schedulePeriod}
+          setEditPeriod={setEditPeriod}
+          setDeletePeriod={setDeletePeriod}
+        />
       )}
       <div className="flex flex-col px-4 py-2">
         {role == "teacher" ? (
@@ -205,11 +202,13 @@ const SubjectSchedulePeriod = ({
 };
 
 const PeriodHoverMenu = ({
+  show: givenShow,
   day,
   schedulePeriod,
   setEditPeriod,
   setDeletePeriod,
 }: {
+  show: boolean;
   day: Day;
   schedulePeriod: SchedulePeriodType;
   setEditPeriod?: ({
@@ -229,67 +228,149 @@ const PeriodHoverMenu = ({
     periodID: number;
   }) => void;
 }): JSX.Element => {
+  const router = useRouter();
+
+  const [listeningCursor, setListeningCursor] = useState<boolean>(false);
+  const [cursorStart, setCursorStart] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
+  const [cursor, setCursor] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
+  const [show, setShow] = useState<boolean>(givenShow);
+
+  useEffect(() => setShow(givenShow), [givenShow]);
+
+  // Component-specific utils
+  function findNumPeriodsFromCursor(
+    initial: number,
+    xStart: number,
+    xEnd: number
+  ): number {
+    return initial + Math.floor((xEnd - xStart) / 112);
+  }
+
   return (
-    <motion.div
-      className="pointer-events-none absolute h-full w-full rounded-lg bg-secondary-translucent-12
-        outline-offset-0 outline-primary"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={animationTransition}
-    >
-      <div className="relative h-full w-full">
-        {/* Edit/delete group */}
-        <div
-          className="surface absolute top-0 left-1/2 flex w-fit -translate-x-1/2 -translate-y-1/2 flex-row gap-0.5
-          overflow-hidden rounded-full"
-        >
-          {/* Edit button */}
-          <button
-            className="primary pointer-events-auto p-1 text-xl shadow"
-            onClick={
-              setEditPeriod
-                ? () => setEditPeriod({ show: true, day, schedulePeriod })
-                : undefined
-            }
+    <>
+      <AnimatePresence>
+        {show && (
+          <motion.div
+            className="pointer-events-none absolute z-30 h-full w-full rounded-lg
+              bg-secondary-translucent-12 outline-offset-0 outline-primary"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={animationTransition}
           >
-            <MaterialIcon icon="edit" allowCustomSize />
-          </button>
+            <div className="relative h-full w-full">
+              {/* Edit/delete group */}
+              <div
+                className="surface absolute top-0 left-1/2 flex w-fit -translate-x-1/2 -translate-y-1/2 flex-row gap-0.5
+                overflow-hidden rounded-full"
+              >
+                {/* Edit button */}
+                <button
+                  className="primary pointer-events-auto p-1 text-xl shadow"
+                  onClick={
+                    setEditPeriod
+                      ? () => setEditPeriod({ show: true, day, schedulePeriod })
+                      : undefined
+                  }
+                >
+                  <MaterialIcon icon="edit" allowCustomSize />
+                </button>
 
-          {/* Delete button */}
-          <button
-            className="error pointer-events-auto p-1 text-xl shadow"
-            onClick={
-              setDeletePeriod
-                ? () =>
-                    setDeletePeriod({
-                      show: true,
-                      periodID: schedulePeriod.id || 0,
-                    })
-                : undefined
-            }
-          >
-            <MaterialIcon icon="delete" allowCustomSize />
-          </button>
-        </div>
+                {/* Delete button */}
+                <button
+                  className="error pointer-events-auto p-1 text-xl shadow"
+                  onClick={
+                    setDeletePeriod
+                      ? () =>
+                          setDeletePeriod({
+                            show: true,
+                            periodID: schedulePeriod.id || 0,
+                          })
+                      : undefined
+                  }
+                >
+                  <MaterialIcon icon="delete" allowCustomSize />
+                </button>
+              </div>
 
-        {/* Drag handle */}
-        <button
-          className="surface pointer-events-auto absolute top-1/2 left-0 w-fit
-           -translate-x-1/2 -translate-y-1/2 cursor-move rounded-full p-1 text-xl shadow"
-        >
-          <MaterialIcon icon="drag_indicator" allowCustomSize />
-        </button>
+              {/* Drag handle */}
+              <button
+                className="surface pointer-events-auto absolute top-1/2 left-0 w-fit
+                -translate-x-1/2 -translate-y-1/2 cursor-move rounded-full p-1 text-xl shadow"
+              >
+                <MaterialIcon icon="drag_indicator" allowCustomSize />
+              </button>
 
-        {/* Resize handle */}
-        <button
-          className="surface pointer-events-auto absolute top-1/2 left-full w-fit
-            -translate-x-1/2 -translate-y-1/2 cursor-ew-resize rounded-full p-1 text-xl shadow"
-        >
-          <MaterialIcon icon="straighten" allowCustomSize />
-        </button>
-      </div>
-    </motion.div>
+              {/* Resize handle */}
+              <button
+                className="surface pointer-events-auto absolute top-1/2 left-full w-fit
+                  -translate-x-1/2 -translate-y-1/2 cursor-ew-resize rounded-full p-1 text-xl shadow"
+                onMouseDown={(e) => {
+                  setListeningCursor(true);
+                  setCursorStart({ x: e.clientX, y: e.clientY });
+                  setCursor({ x: e.clientX, y: e.clientY });
+                }}
+              >
+                <MaterialIcon icon="straighten" allowCustomSize />
+              </button>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {listeningCursor && (
+          <>
+            {/* Visual indicator */}
+            <motion.div
+              className="absolute top-0 left-0 h-[3.75rem] rounded-lg border-4 border-tertiary transition-[width]"
+              style={{
+                width:
+                  findNumPeriodsFromCursor(
+                    schedulePeriod.duration,
+                    cursorStart.x,
+                    cursor.x
+                  ) *
+                    // Multiply no. of periods by a period’s pixel length
+                    112 -
+                  // Correct for 4px border (both sides)
+                  8,
+              }}
+              initial={{ opacity: 0, borderWidth: 2 }}
+              animate={{ opacity: 1, borderWidth: 4 }}
+              exit={{ opacity: 0, borderWidth: 2 }}
+              transition={animationTransition}
+            />
+
+            {/* Detection area */}
+            <div
+              className="absolute top-0 left-0 h-[3.75rem] cursor-ew-resize rounded-lg bg-tertiary-translucent-12 sm:bg-transparent"
+              style={{ width: (10 - schedulePeriod.startTime + 1) * 112 }}
+              onMouseMove={(e) => setCursor({ x: e.clientX, y: e.clientY })}
+              onMouseUp={() => {
+                setListeningCursor(false);
+                router.replace(router.asPath);
+              }}
+              onTouchEnd={(e) => {
+                setCursor({
+                  x: (e.touches[0] || e.changedTouches[0]).pageX,
+                  y: (e.touches[0] || e.changedTouches[0]).pageY,
+                });
+                setTimeout(() => {
+                  setListeningCursor(false);
+                  router.replace(router.asPath);
+                }, 2000);
+              }}
+            />
+          </>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
