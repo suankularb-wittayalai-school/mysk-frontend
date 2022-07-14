3c7b76e1f3c8e1d344d5d324c3b27257de33600f
jimmy-tempest <smartwattana@gmail.com>
2022-07-14 23:39:40 +0700

    feat: add form types for db

---
+++ src/utils/types/database/form.ts
@@ -0,0 +1,26 @@
+import { FieldType } from "../form";
+
+export interface FormTable {
+  id: number;
+  created_at: Date;
+  name_th: string;
+  name_en: string;
+  description_th: string;
+  description_en: string;
+  parent: number;
+  due_date: Date;
+  students_done: number;
+  frequency: "once" | "weekly" | "monthly" | "unspecified";
+}
+
+export interface FormQuestionsTable {
+  id: number;
+  form: number;
+  label_th: string;
+  label_en?: string;
+  type: FieldType;
+  options: string[];
+  required: boolean;
+  range_start: number;
+  range_end: number;
+}
