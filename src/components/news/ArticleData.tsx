837716def1e6fd997627fbb25251f9973b6e52ad
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-22 01:47:02 +0700

    feat: wrap new table options in Card

--- src/components/news/ArticleData.tsx
+++ src/components/news/ArticleData.tsx
@@ -22,52 +22,81 @@ const ArticleData = (): JSX.Element => {
         icon={<MaterialIcon icon="table" allowCustomSize />}
         text="Data"
       />
-      <LayoutGridCols cols={3}>
-        <Card type="stacked" appearance="outlined">
+      <div>
+        <Card type="stacked" appearance="tonal">
           <CardHeader
-            icon={<MaterialIcon icon="folder_open" />}
-            title={<h3>Import spreadsheet</h3>}
-            label={<span>Display a file as a table</span>}
+            icon={<MaterialIcon icon="add_circle" />}
+            title={<h3>Create new table</h3>}
           />
-          <CardSupportingText>
-            <p>
-              Use a .csv or a .xlsx file as data to be displayed to the user.
-            </p>
-            <FileInput name="spreadsheet-file" label="Spreadsheet file" />
+
+          <CardSupportingText className="!pt-0">
+            <LayoutGridCols cols={3}>
+              {/* Import spreadsheet */}
+              <Card type="stacked" appearance="tonal" className="!bg-surface">
+                <CardHeader
+                  icon={<MaterialIcon icon="folder_open" />}
+                  title={<h4>Import spreadsheet</h4>}
+                  label={<span>Display a file as a table</span>}
+                />
+                <CardSupportingText>
+                  <p>
+                    Use a .csv or a .xlsx file as data to be displayed to the
+                    user.
+                  </p>
+                  <FileInput
+                    name="spreadsheet-file"
+                    label="Spreadsheet file"
+                    attr={{
+                      accept:
+                        ".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    }}
+                  />
+                </CardSupportingText>
+              </Card>
+
+              {/* Create Markdown table */}
+              <Card type="stacked" appearance="tonal" className="!bg-surface">
+                <CardHeader
+                  icon={<MaterialIcon icon="edit_square" />}
+                  title={<h4>Create Markdown table</h4>}
+                  label={<span>Use the Markdown syntax</span>}
+                />
+                <CardSupportingText>
+                  <TextArea
+                    name="markdown"
+                    label="Markdown"
+                    onChange={() => {}}
+                  />
+                  <Markdown noStyles>{""}</Markdown>
+                </CardSupportingText>
+              </Card>
+
+              {/* Link form */}
+              <Card type="stacked" appearance="tonal" className="!bg-surface">
+                <CardHeader
+                  icon={<MaterialIcon icon="link" />}
+                  title={<h4>Link form</h4>}
+                  label={<span>Tally student responses</span>}
+                />
+                <CardSupportingText>
+                  <p>
+                    Enter the link to the form from which you’d like to tally
+                    the student responses. We only support forms made in MySK.
+                  </p>
+                  <KeyboardInput
+                    name="form-link"
+                    type="url"
+                    label="Form link"
+                    helperMsg="Starts with “https://mysk.school/news/form/”."
+                    onChange={() => {}}
+                    attr={{ pattern: "https://mysk.school/news/form/\\d+" }}
+                  />
+                </CardSupportingText>
+              </Card>
+            </LayoutGridCols>
           </CardSupportingText>
         </Card>
-        <Card type="stacked" appearance="outlined">
-          <CardHeader
-            icon={<MaterialIcon icon="edit_square" />}
-            title={<h3>Create Markdown table</h3>}
-            label={<span>Use the Markdown syntax</span>}
-          />
-          <CardSupportingText>
-            <TextArea name="markdown" label="Markdown" onChange={() => {}} />
-            <Markdown noStyles>{""}</Markdown>
-          </CardSupportingText>
-        </Card>
-        <Card type="stacked" appearance="outlined">
-          <CardHeader
-            icon={<MaterialIcon icon="link" />}
-            title={<h3>Link form</h3>}
-            label={<span>Tally student responses</span>}
-          />
-          <CardSupportingText>
-            <p>
-              Enter the link to the form from which you’d like to tally the
-              student responses. We only support forms made in MySK.
-            </p>
-            <KeyboardInput
-              name="form-link"
-              type="url"
-              label="Form link"
-              helperMsg="Starts with “https://mysk.school/form/”."
-              onChange={() => {}}
-            />
-          </CardSupportingText>
-        </Card>
-      </LayoutGridCols>
+      </div>
     </Section>
   );
 };
