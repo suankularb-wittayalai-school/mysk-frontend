d8ffa97df6eb0a11882773e1cac66b94543f70e5
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-28 04:44:09 +0700

    feat: Form page prep

---
+++ src/pages/news/form/[formID].tsx
@@ -0,0 +1,44 @@
+// External libraries
+import { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+// Backend
+import { getForm } from "@utils/backend/news/form";
+
+// Type
+import { FormPage as FormPageType } from "@utils/types/news";
+import { LangCode } from "@utils/types/common";
+
+const FormPage: NextPage<{ form: FormPageType }> = ({ form }) => {
+  const { t } = useTranslation("common");
+  const locale = useRouter().locale as LangCode;
+
+  console.log(form);
+
+  // TODO: Render Form
+  return <div>Form page</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  params,
+}) => {
+  if (!params?.formID) return { notFound: true };
+
+  const { data: form, error } = await getForm(Number(params?.formID));
+  if (error) return { notFound: true };
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as LangCode, ["common"])),
+      form,
+    },
+  };
+};
+
+export default FormPage;
