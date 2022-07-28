462f936b9158c5a1296de4df623dde0823e121e2
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-28 18:02:09 +0700

    feat: basic form elements render

--- src/pages/news/form/[formID].tsx
+++ src/pages/news/form/[formID].tsx
@@ -19,10 +19,29 @@ import { createTitleStr } from "@utils/helpers/title";
 // Types
 import { LangCode } from "@utils/types/common";
 import { FormPage as FormPageType } from "@utils/types/news";
+import { protectPageFor } from "@utils/helpers/route";
+import {
+  Dropdown,
+  KeyboardInput,
+  NativeInput,
+  TextArea,
+} from "@suankularb-components/react";
 
 const FormPage: NextPage<{ form: FormPageType }> = ({ form }) => {
   const { t } = useTranslation("common");
   const locale = useRouter().locale as LangCode;
+
+  const fieldTypes = [
+    "short_answer",
+    "paragraph",
+    "multiple_choice",
+    "check_box",
+    "dropdown",
+    "file",
+    "date",
+    "time",
+    "scale",
+  ];
 
   // TODO: Render Form
   return (
@@ -33,10 +52,51 @@ const FormPage: NextPage<{ form: FormPageType }> = ({ form }) => {
         </title>
       </Head>
       <NewsPageWrapper news={form}>
-        <section className="mt-12">
-          {form.content.fields.map((field) => (
-            <div key={field.id}>{JSON.stringify(field)}</div>
-          ))}
+        <section className="mt-12 flex flex-col items-center">
+          {form.content.fields.map((field) =>
+            // Short answer
+            field.type == "short_answer" ? (
+              <KeyboardInput
+                key={field.id}
+                name={getLocaleString(field.label, locale)}
+                type="text"
+                label={getLocaleString(field.label, locale)}
+                onChange={() => {}}
+                className="w-[calc(33.33%-0.6666rem)]"
+              />
+            ) : // Paragraph
+            field.type == "paragraph" ? (
+              <TextArea
+                key={field.id}
+                name={getLocaleString(field.label, locale)}
+                label={getLocaleString(field.label, locale)}
+                onChange={() => {}}
+                className="w-[calc(33.33%-0.6666rem)]"
+              />
+            ) : // Date and time
+            ["date", "time"].includes(field.type) ? (
+              <NativeInput
+                key={field.id}
+                name={getLocaleString(field.label, locale)}
+                type={field.type as "date" | "time"}
+                label={getLocaleString(field.label, locale)}
+                onChange={() => {}}
+                className="w-[calc(33.33%-0.6666rem)]"
+              />
+            ) : // Dropdown
+            field.type == "dropdown" ? (
+              <Dropdown
+                key={field.id}
+                name={getLocaleString(field.label, locale)}
+                label={getLocaleString(field.label, locale)}
+                options={(field.options as string[]).map((option) => ({
+                  value: option,
+                  label: option,
+                }))}
+                className="w-[calc(33.33%-0.6666rem)]"
+              />
+            ) : null
+          )}
         </section>
       </NewsPageWrapper>
     </>
@@ -46,7 +106,11 @@ const FormPage: NextPage<{ form: FormPageType }> = ({ form }) => {
 export const getServerSideProps: GetServerSideProps = async ({
   locale,
   params,
+  req,
 }) => {
+  const redirect = await protectPageFor("student", req);
+  if (redirect) return redirect;
+
   if (!params?.formID) return { notFound: true };
 
   const { data: form, error } = await getForm(Number(params?.formID));
