eb7760c92dc7142b61c35804018c7843c0e03c63
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-29 02:57:07 +0700

    fix: unnecessary any in News Feed

--- src/components/news/NewsFeed.tsx
+++ src/components/news/NewsFeed.tsx
@@ -9,7 +9,7 @@ import NewsCard from "@components/news/NewsCard";
 import { animationTransition } from "@utils/animations/config";
 
 // Types
-import { NewsItemNoDate, NewsListNoDate } from "@utils/types/news";
+import { NewsItemNoDate } from "@utils/types/news";
 
 const NewsFeed = ({
   news,
@@ -29,37 +29,25 @@ const NewsFeed = ({
           className="flex flex-row gap-4 sm:gap-6"
           columnClassName="flex flex-col gap-4 sm:gap-6"
         >
-          {news
-            .map((newsItem) => ({
-              ...newsItem,
-              postDate: new Date(newsItem.postDate),
-              dueDate:
-                newsItem.type == "form" || newsItem.type == "payment"
-                  ? newsItem.dueDate
-                  : undefined,
-            }))
-            .map((newsItem, index) => (
-              <motion.article
-                key={newsItem.id}
-                aria-posinset={index}
-                aria-setsize={-1}
-                initial={{ scale: 0.8, y: 20, opacity: 0 }}
-                animate={{ scale: 1, y: 0, opacity: 1 }}
-                exit={{ scale: 0.8, y: 20, opacity: 0 }}
-                layoutId={["news", newsItem.type, newsItem.id].join("-")}
-                transition={animationTransition}
-              >
-                <NewsCard
-                  // (@SiravitPhokeed)
-                  // I have no idea what’s wro here so…um…apologies
-                  // for the `any`.
-                  newsItem={newsItem as any}
-                  editable={isForAdmin}
-                  btnType={btnType || "filled"}
-                  showChips
-                />
-              </motion.article>
-            ))}
+          {news.map((newsItem, index) => (
+            <motion.article
+              key={newsItem.id}
+              aria-posinset={index}
+              aria-setsize={-1}
+              initial={{ scale: 0.8, y: 20, opacity: 0 }}
+              animate={{ scale: 1, y: 0, opacity: 1 }}
+              exit={{ scale: 0.8, y: 20, opacity: 0 }}
+              layoutId={["news", newsItem.type, newsItem.id].join("-")}
+              transition={animationTransition}
+            >
+              <NewsCard
+                newsItem={newsItem}
+                editable={isForAdmin}
+                btnType={btnType || "filled"}
+                showChips
+              />
+            </motion.article>
+          ))}
         </Masonry>
       </AnimatePresence>
     </LayoutGroup>
