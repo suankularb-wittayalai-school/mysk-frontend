b2f298b7ca1e7f11c5721158f3cb287c557e40cc
Sadudee Theparree <78859665+ImSadudee@users.noreply.github.com>
2022-10-31 20:28:36 +0700

    feat: og tags for news

--- src/components/news/NewsPageWrapper.tsx
+++ src/components/news/NewsPageWrapper.tsx
@@ -29,6 +29,7 @@ import { useSession } from "@utils/hooks/auth";
 // Types
 import { LangCode } from "@utils/types/common";
 import { FormPage, InfoPage } from "@utils/types/news";
+import Head from "next/head";
 
 const NewsPageWrapper = ({
   news,
@@ -44,62 +45,70 @@ const NewsPageWrapper = ({
   return (
     // We’re not using ReSKComs in parts of this page here as it has poor
     // support for Framer Motion’s layout animations.
-    <main className="content-layout">
-      <Title
-        name={{
-          title: getLocaleString(news.content.title, locale),
-        }}
-        pageIcon={<MaterialIcon icon="info" />}
-        backGoesTo={session ? "/news" : "/"}
-        LinkElement={Link}
-      />
-      <div className="content-layout__content !gap-y-0">
-        {/* This part will animate from News Card and Landing Feed Item. */}
-        <motion.section
-          className="section"
-          layoutId={["news", news.type, news.id].join("-")}
-          transition={enterPageTransition}
-        >
-          {/* Banner image */}
-          <div
-            className="container-surface-variant relative aspect-video w-full
-              overflow-hidden !p-0 text-right shadow sm:rounded-xl md:aspect-[5/1]"
+    <>
+      <Head>
+        <meta property="og:title" content={getLocaleString(news.content.title, locale)} />
+        <meta property="og:type" content="news" />
+        <meta property="og:url" content={`/news/info/${news.id}`} />
+        <meta property="og:image" content={news.image} />
+      </Head>
+      <main className="content-layout">
+        <Title
+          name={{
+            title: getLocaleString(news.content.title, locale),
+          }}
+          pageIcon={<MaterialIcon icon="info" />}
+          backGoesTo={session ? "/news" : "/"}
+          LinkElement={Link}
+        />
+        <div className="content-layout__content !gap-y-0">
+          {/* This part will animate from News Card and Landing Feed Item. */}
+          <motion.section
+            className="section"
+            layoutId={["news", news.type, news.id].join("-")}
+            transition={enterPageTransition}
           >
-            {news.image ? (
-              <Image src={news.image} layout="fill" objectFit="cover" alt="" />
-            ) : (
-              <p className="m-4 font-display text-8xl font-light leading-none opacity-30">
+            {/* Banner image */}
+            <div
+              className="container-surface-variant relative aspect-video w-full
+                overflow-hidden !p-0 text-right shadow sm:rounded-xl md:aspect-[5/1]"
+            >
+              {news.image ? (
+                <Image src={news.image} layout="fill" objectFit="cover" alt="" />
+              ) : (
+                <p className="m-4 font-display text-8xl font-light leading-none opacity-30">
+                  {getLocaleString(news.content.title, locale)}
+                </p>
+              )}
+            </div>
+
+            {/* Title and short description */}
+            <div className="font-display">
+              <motion.h1 className="text-4xl font-bold">
                 {getLocaleString(news.content.title, locale)}
-              </p>
-            )}
-          </div>
+              </motion.h1>
+              <motion.p className="text-lg">
+                {getLocaleString(news.content.description, locale)}
+              </motion.p>
 
-          {/* Title and short description */}
-          <div className="font-display">
-            <motion.h1 className="text-4xl font-bold">
-              {getLocaleString(news.content.title, locale)}
-            </motion.h1>
-            <motion.p className="text-lg">
-              {getLocaleString(news.content.description, locale)}
-            </motion.p>
+              {session?.user?.user_metadata.isAdmin && (
+                <Actions className="my-4">
+                  <LinkButton
+                    label={t("pageAction.admin.edit")}
+                    type="tonal"
+                    icon={<MaterialIcon icon="edit" />}
+                    url={`/admin/news/edit/${news.type}/${news.id}`}
+                    LinkElement={Link}
+                  />
+                </Actions>
+              )}
+            </div>
+          </motion.section>
 
-            {session?.user?.user_metadata.isAdmin && (
-              <Actions className="my-4">
-                <LinkButton
-                  label={t("pageAction.admin.edit")}
-                  type="tonal"
-                  icon={<MaterialIcon icon="edit" />}
-                  url={`/admin/news/edit/${news.type}/${news.id}`}
-                  LinkElement={Link}
-                />
-              </Actions>
-            )}
-          </div>
-        </motion.section>
-
-        {children}
-      </div>
-    </main>
+          {children}
+        </div>
+      </main>
+    </>
   );
 };
 
