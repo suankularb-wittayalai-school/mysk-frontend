f74d3b0be100653e196c0e8fc64702ccd17a3f2a
Siravit Phokeed <siravitsasa@gmail.com>
2022-11-17 10:12:53 +0700

    fix: /learn redirect includes /th/

--- src/pages/learn.tsx
+++ src/pages/learn.tsx
@@ -1,26 +1,40 @@
 // External libraries
-import { GetServerSideProps, NextPage } from "next";
-import { User, withPageAuth } from "@supabase/auth-helpers-nextjs";
+import {
+  GetServerSideProps,
+  NextApiRequest,
+  NextApiResponse,
+  NextPage,
+} from "next";
+import {
+  createServerSupabaseClient,
+  User,
+} from "@supabase/auth-helpers-nextjs";
 
 // Backend
 import { getClassNumberFromUser } from "@utils/backend/classroom/classroom";
 
 const Learn: NextPage = () => null;
 
-export const getServerSideProps: GetServerSideProps = withPageAuth({
-  async getServerSideProps({ locale }, supabase) {
-    const { data: user } = await supabase.auth.getUser();
-    const { data: classID } = await getClassNumberFromUser(
-      supabase,
-      user.user as User
-    );
-    return {
-      redirect: {
-        destination: `/${locale}/learn/${classID}`,
-        permanent: false,
-      },
-    };
-  },
-});
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  req,
+  res,
+}) => {
+  const supabase = createServerSupabaseClient({
+    req: req as NextApiRequest,
+    res: res as NextApiResponse,
+  });
+  const { data: user } = await supabase.auth.getUser();
+  const { data: classID } = await getClassNumberFromUser(
+    supabase,
+    user.user as User
+  );
+  return {
+    redirect: {
+      destination: `${locale == "th" ? "" : "/en-US"}/learn/${classID}`,
+      permanent: false,
+    },
+  };
+};
 
 export default Learn;
