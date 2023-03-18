d82a8a82bd77c7228d7a6f10530b3c1a03e5d25d
Siravit Phokeed <siravitsasa@gmail.com>
2023-03-19 00:03:36 +0700

    feat: bundle analyzer setup

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@
 
 const { i18n } = require("./next-i18next.config");
 
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env?.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ["ykqqepbodqjhiwfjcvxe.supabase.co"],
   },
   i18n,
-};
+});
