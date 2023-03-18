cf547c726a4a4cccc3ac19f17faa974a1229e06c
Siravit Phokeed <siravitsasa@gmail.com>
2023-03-18 13:16:49 +0700

    fix: language switcher always route to Landing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -253,7 +253,7 @@ const Layout: FC<
                 <NavBarItem
                   icon={<MaterialIcon icon="translate" />}
                   label={t("navigation.language")}
-                  href="/"
+                  href={router.pathname}
                   element={(props) => (
                     <Link
                       locale={router.locale == "en-US" ? "th" : "en-US"}
