9c81f5a15a3da86605900c27123e2bf8e1cffc5e
Siravit Phokeed <siravitsasa@gmail.com>
2023-04-04 16:45:26 +0700

    fix: class and room regex only match part

--- utils/patterns.ts
+++ utils/patterns.ts
@@ -2,10 +2,10 @@ export const schoolEmailPattern = "((@sk.ac.th)|(@student.sk.ac.th))$";
 export const schoolEmailRegex = /((@sk.ac.th)|(@student.sk.ac.th))$/;
 
 export const classPattern = "[1-6](0[1-9]|1[0-9])";
-export const classRegex = /[1-6](0[1-9]|1[0-9])/;
+export const classRegex = /^[1-6](0[1-9]|1[0-9])$/;
 
 export const roomPattern = "[1-6][1-5]\\d{2}";
-export const roomRegex = /[1-6][1-5]\d{2}/;
+export const roomRegex = /^[1-6][1-5]\d{2}$/;
 
 export const subjectCodePattern = "([\u0E00-\u0E7FA-Z]|[A-Z]{1,3})\\d{5}";
 export const subjectCodeTHPattern = "[\u0E00-\u0E7FA-Z]\\d{5}";
