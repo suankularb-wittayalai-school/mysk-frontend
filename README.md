c34a5c83c663c7dddf4481cbcd698f93bcc55386
Siravit Phokeed <siravitsasa@gmail.com>
2023-04-05 16:49:16 +0700

    feat: PWA

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # MySK
 
 Website dedicated to facilitating Suankularb Wittayalai school in collection and access of data.
+
+## Development
+
+Start the development server with this command at the project root:
+
+```
+npm run dev
+```
+
+To build MySK and run it in production on your marchine, run these commands:
+
+```
+npm run build
+npm start
+```
+
+With the production build running, you can also install MySK as a Progressive Web App.
+
+> **Note:** when running the build locally, you can only access it via your IPv4 address with the 3000 port. `http://127.0.0.1:3000` will not work. You can find your address by running `ipconfig` in the terminal.
--- next.config.js
+++ next.config.js
@@ -6,18 +6,33 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env?.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-  images: {
-    domains: ["ykqqepbodqjhiwfjcvxe.supabase.co"],
-  },
-  i18n,
-  async redirects() {
-    return [
-      { source: "/account/login", destination: "/", permanent: true },
-      { source: "/welcome", destination: "/account/welcome", permanent: true },
-      { source: "/learn/:id", destination: "/learn", permanent: true },
-      { source: "/admin", destination: "/lookup", permanent: true },
-    ];
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  // (@SiravitPhokeed)
+  // The docs says that `cacheOnFrontEndNav` should be enabled if the user has
+  // spotty internet connection. Since we plan to deploy this for a school
+  // with...imperfect wi-fi, it’s enabled.
+  cacheOnFrontEndNav: true,
+  disable: process.env.NODE_ENV === "development",
 });
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    images: { domains: ["ykqqepbodqjhiwfjcvxe.supabase.co"] },
+    i18n,
+    async redirects() {
+      return [
+        { source: "/account/login", destination: "/", permanent: true },
+        {
+          source: "/welcome",
+          destination: "/account/welcome",
+          permanent: true,
+        },
+        { source: "/learn/:id", destination: "/learn", permanent: true },
+        { source: "/admin", destination: "/lookup", permanent: true },
+      ];
+    },
+  })
+);
