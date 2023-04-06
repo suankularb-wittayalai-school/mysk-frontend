ef0003fba2694e160b37788ca777485dd5daed02
Siravit Phokeed <siravitsasa@gmail.com>
2023-04-06 10:43:38 +0700

    fix: build errors

--- utils/helpers/title.ts
+++ utils/helpers/title.ts
@@ -1,6 +1,5 @@
 import { TFunction } from "next-i18next";
 
 export function createTitleStr(pageName: string, t: TFunction) {
-  if (window.matchMedia("(display-mode: standalone)").matches) return pageName;
   return `${pageName} - ${t("brand.name", { ns: "common" })}`;
 }
