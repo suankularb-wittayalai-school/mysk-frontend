7da48cbba7f87e9f82ca783cbea17aeb23fb892d
Siravit Phokeed <siravitsasa@gmail.com>
2023-04-06 10:27:37 +0700

    fix: MySK shown twice in PWA tab name

--- utils/helpers/title.ts
+++ utils/helpers/title.ts
@@ -1,5 +1,6 @@
 import { TFunction } from "next-i18next";
 
 export function createTitleStr(pageName: string, t: TFunction) {
+  if (window.matchMedia("(display-mode: standalone)").matches) return pageName;
   return `${pageName} - ${t("brand.name", { ns: "common" })}`;
 }
