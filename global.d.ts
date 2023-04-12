e4324d1bf55c83be1ec089aba37d5923cf0fe148
Siravit Phokeed <siravitsasa@gmail.com>
2023-04-12 21:23:25 +0700

    refactor: update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -5,6 +5,8 @@ declare global {
       NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
       SUPABASE_SERVICE_ROLE: string;
       NEW_ACCOUNT_DEFAULT_PASS: string;
+      DISABLE_PWA: string;
+      ANALYZE: string;
     }
   }
 }
