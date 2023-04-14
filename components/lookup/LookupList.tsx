0407b8d45fa11067818ebec0dbb49f9bbc378772
Siravit Phokeed <siravitsasa@gmail.com>
2023-04-14 14:52:30 +0700

    refactor: allow controlled Search in Lookup List

--- components/lookup/LookupList.tsx
+++ components/lookup/LookupList.tsx
@@ -14,25 +14,36 @@ import { useLocale } from "@/utils/hooks/i18n";
  * @param children The list content.
  * @param length How many items in the list.
  * @param searchAlt The alt text for Search.
- * @param query Search’s field value, as Search is a controlled component.
- * @param setQuery Triggered when Search’s field value changes.
+ * @param query Search’s field value, if you want Search to be a controlled component.
+ * @param onQueryChange Triggered when Search’s field value changes.
+ * @param onSearch Triggered when the Search Button is pressed.
+ * @param liveFilter If the list is updated live as the user type. If this is `false`, the user is informed that they need to press the Search Button to perform the search.
  */
 const LookupList: FC<{
   children: ReactNode;
   length: number;
   searchAlt: string;
   searchFilters?: ReactNode;
-  onSearch: (value: string) => void;
-}> = ({ children, length, searchAlt, searchFilters, onSearch }) => {
+  query?: string;
+  onQueryChange?: (value: string) => void;
+  onSearch?: (value: string) => void;
+  liveFilter?: boolean;
+}> = ({
+  children,
+  length,
+  searchAlt,
+  searchFilters,
+  query,
+  onQueryChange,
+  onSearch,
+  liveFilter,
+}) => {
   // Translation
   const locale = useLocale();
   const { t } = useTranslation("lookup", { keyPrefix: "common.list" });
 
-  // Query
-  const [query, setQuery] = useState<string>("");
-  useEffect(() => {
-    if (!query) onSearch("");
-  }, [query]);
+  // Form control
+  const [internalQuery, setInternalQuery] = useState<string>("");
 
   return (
     <aside aria-labelledby="header-list" className="flex flex-col gap-3 !pt-0">
@@ -47,34 +58,50 @@ const LookupList: FC<{
       >
         <Search
           alt={searchAlt}
-          value={query}
+          value={query || internalQuery}
           locale={locale}
-          onChange={setQuery}
-          onSearch={() => onSearch(query)}
+          onChange={(value) => {
+            // Reset the list immediately as the user removes the query
+            if (onSearch && value === "") onSearch("");
+            // (Controlled) Trigger `onQueryChange`
+            if (query !== undefined && onQueryChange) {
+              onQueryChange(value);
+              return;
+            }
+            // (Apparent uncontrolled)
+            // Trigger `onQueryChange` AND update internal form control
+            if (onQueryChange) onQueryChange(value);
+            setInternalQuery(value);
+          }}
+          onSearch={() => onSearch && onSearch(query || internalQuery)}
         >
-          <p className="px-4 text-on-surface-variant">{t("searchHelper")}</p>
+          {!liveFilter && (
+            <p className="px-4 text-on-surface-variant">{t("searchHelper")}</p>
+          )}
         </Search>
         {searchFilters}
       </div>
 
       {/* List */}
-      <div className="flex flex-col gap-2">
+      <ul className="flex flex-col gap-2">
         {/* List content */}
         {children}
 
         {/* Card at the end to explain why the list has stopped */}
         {(length === 0 || length > 10) && (
-          <Card appearance="outlined">
-            <p className="py-2 px-4 text-on-surface-variant">
-              {length === 100
-                ? t("maxNote")
-                : length === 0
-                ? t("noResults")
-                : t("endOfList")}
-            </p>
-          </Card>
+          <li>
+            <Card appearance="outlined">
+              <p className="py-2 px-4 text-on-surface-variant">
+                {length === 100
+                  ? t("maxNote")
+                  : length === 0
+                  ? t("noResults")
+                  : t("endOfList")}
+              </p>
+            </Card>
+          </li>
         )}
-      </div>
+      </ul>
     </aside>
   );
 };
