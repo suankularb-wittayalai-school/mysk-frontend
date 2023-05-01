62284743f7c152d5059d0401f811ce8d0e6a8cc7
Siravit Phokeed <siravitsasa@gmail.com>
2023-05-01 13:26:03 +0700

    fix: nameJoiner incompatible with foreign teachers

--- components/common/MultilingualText.tsx
+++ components/common/MultilingualText.tsx
@@ -11,49 +11,69 @@ const MultilangText: FC<{
   text: MultiLangString;
   options?: Partial<{
     hideEmptyLanguage: boolean;
+    hideIconsIfOnlyLanguage: boolean;
     priorityLanguage: LangCode;
   }>;
   className?: string;
-}> = ({ text, options, className }) => (
-  <div className={cn(["grid grid-cols-[1.25rem,1fr] gap-1", className])}>
-    {(
-      (options?.priorityLanguage === "en-US"
-        ? ["en-US", "th"]
-        : ["th", "en-US"]) as LangCode[]
-    ).map(
-      (langCode) =>
-        !(options?.hideEmptyLanguage && !text[langCode]) && (
-          <Fragment key={langCode}>
-            {/* Icon */}
-            <div
-              aria-label={langCode === "en-US" ? "English" : "ภาษาไทย"}
-              className={cn([
-                `grid h-5 w-5 select-none place-content-center rounded-full
-                 border-1 text-[0.5rem]`,
-                !options?.priorityLanguage ||
-                langCode === options?.priorityLanguage
-                  ? `border-secondary text-secondary`
-                  : `border-outline text-outline`,
-              ])}
-            >
-              {langCode === "en-US" ? "EN" : "TH"}
-            </div>
+}> = ({ text, options, className }) => {
+  const numLanguagesWithContent = (Object.keys(text) as LangCode[]).filter(
+    (locale) => text[locale]
+  ).length;
 
-            {/* Text */}
-            <p
-              className={
-                options?.priorityLanguage &&
-                langCode !== options?.priorityLanguage
-                  ? `text-outline`
-                  : undefined
-              }
-            >
-              {text[langCode]}
-            </p>
-          </Fragment>
-        )
-    )}
-  </div>
-);
+  return (
+    <div className={cn(["grid grid-cols-[1.25rem,1fr] gap-1", className])}>
+      {(
+        (options?.priorityLanguage === "en-US"
+          ? ["en-US", "th"]
+          : ["th", "en-US"]) as LangCode[]
+      ).map(
+        (langCode) =>
+          !(options?.hideEmptyLanguage && !text[langCode]) && (
+            <Fragment key={langCode}>
+              {/* Icon */}
+              {!(
+                // Only show the icon if there is at least 2 languages
+                (
+                  options?.hideIconsIfOnlyLanguage &&
+                  numLanguagesWithContent === 1
+                )
+              ) && (
+                <div
+                  aria-label={langCode === "en-US" ? "English" : "ภาษาไทย"}
+                  className={cn([
+                    `grid h-5 w-5 select-none place-content-center rounded-full
+                     border-1 text-[0.5rem]`,
+                    !options?.priorityLanguage ||
+                    langCode === options?.priorityLanguage
+                      ? `border-secondary text-secondary`
+                      : `border-outline text-outline`,
+                  ])}
+                >
+                  {langCode === "en-US" ? "EN" : "TH"}
+                </div>
+              )}
+
+              {/* Text */}
+              <p
+                className={cn([
+                  options?.hideIconsIfOnlyLanguage &&
+                    // Span 2 columns (taking over the icon space) if this is the
+                    // only language
+                    numLanguagesWithContent === 1 &&
+                    `col-span-2`,
+                  options?.priorityLanguage &&
+                    // Dim the text color if this isn’t the priority language
+                    langCode !== options?.priorityLanguage &&
+                    `text-outline`,
+                ])}
+              >
+                {text[langCode]}
+              </p>
+            </Fragment>
+          )
+      )}
+    </div>
+  );
+};
 
 export default MultilangText;
