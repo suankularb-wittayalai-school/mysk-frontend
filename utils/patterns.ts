79573b3766a35b0ed2b2e8cf307dee3d7032901e
Siravit Phokeed <siravitsasa@gmail.com>
2023-05-10 15:46:14 +0700

    fix: room cannot start with 70

--- utils/patterns.ts
+++ utils/patterns.ts
@@ -1,7 +1,7 @@
 export const schoolEmailRegex = /((@sk.ac.th)|(@student.sk.ac.th))$/;
 
 export const classRegex = /^[1-6](0[1-9]|1[0-9])$/;
-export const roomRegex = /^[1-6][1-5]\d{2}(\/[1-9])?$/;
+export const roomRegex = /^([1-6][1-5]|70)\d{2}(\/\d)?$/;
 
 export const subjectCodeRegex = /^([\u0E00-\u0E7FA-Z]|[A-Z]{1,3})\\d{5}$/;
 export const subjectCodeTHRegex = /^[\u0E00-\u0E7FA-Z]\\d{5}$/;
