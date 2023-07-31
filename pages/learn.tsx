0f7ce6a91830c068d620dd5b7268dcfa4a6f4231
Siravit Phokeed <siravitsasa@gmail.com>
2023-07-31 21:31:19 +0700

    refactor: replace any in Learn

--- pages/learn.tsx
+++ pages/learn.tsx
@@ -1,17 +1,32 @@
-// External libraries
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+/**
+ * `/learn` TABLE OF CONTENTS
+ *
+ * Note: `Ctrl` + click to jump to a component.
+ *
+ * **Sections**
+ * - {@link ScheduleSection}
+ * - {@link SubjectListSection}
+ *
+ * **Page**
+ * - {@link LearnPage}
+ */
 
-import { LayoutGroup, motion } from "framer-motion";
-
-import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
-import Head from "next/head";
-
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-
-import { FC, useState } from "react";
-
-// SK Components
+// Imports
+import MySKPageHeader from "@/components/common/MySKPageHeader";
+import Schedule from "@/components/schedule/Schedule";
+import ScheduleAtAGlance from "@/components/schedule/ScheduleAtAGlance";
+import SubjectList from "@/components/subject/SubjectList";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import getLoggedInPerson from "@/utils/backend/account/getLoggedInPerson";
+import getClassSchedule from "@/utils/backend/schedule/getClassSchedule";
+import getClassroomSubjectsOfClass from "@/utils/backend/subject/getClassroomSubjectsOfClass";
+import { createEmptySchedule } from "@/utils/helpers/schedule";
+import { createTitleStr } from "@/utils/helpers/title";
+import { useLocale } from "@/utils/hooks/i18n";
+import { CustomPage, LangCode } from "@/utils/types/common";
+import { Student } from "@/utils/types/person";
+import { Schedule as ScheduleType } from "@/utils/types/schedule";
+import { ClassroomSubject } from "@/utils/types/subject";
 import {
   Columns,
   ContentLayout,
@@ -21,38 +36,21 @@ import {
   transition,
   useAnimationConfig,
 } from "@suankularb-components/react";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import { LayoutGroup, motion } from "framer-motion";
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
+import { FC, useState } from "react";
 
-// Internal components
-import MySKPageHeader from "@/components/common/MySKPageHeader";
-import Schedule from "@/components/schedule/Schedule";
-import ScheduleAtAGlance from "@/components/schedule/ScheduleAtAGlance";
-import SubjectList from "@/components/subject/SubjectList";
-
-// Backend
-// import { getClassFromUser } from "@/utils/backend/classroom/classroom";
-// import { getSchedule } from "@/utils/backend/schedule/schedule";
-// import { getSubjectList } from "@/utils/backend/subject/roomSubject";
-
-// Types
-import { CustomPage, LangCode } from "@/utils/types/common";
-// import { Schedule as ScheduleType } from "@/utils/types/schedule";
-// import { SubjectListItem } from "@/utils/types/subject";
-
-// Helpers
-import { createTitleStr } from "@/utils/helpers/title";
-
-// Hooks
-import getLoggedInPerson from "@/utils/backend/account/getLoggedInPerson";
-import getClassSchedule from "@/utils/backend/schedule/getClassSchedule";
-import { useLocale } from "@/utils/hooks/i18n";
-import { Student } from "@/utils/types/person";
-import { Schedule as ScheduleType } from "@/utils/types/schedule";
-import { authOptions } from "./api/auth/[...nextauth]";
-import { createEmptySchedule } from "@/utils/helpers/schedule";
-import getClassroomSubjectsOfClass from "@/utils/backend/subject/getClassroomSubjectsOfClass";
-import { ClassroomSubject } from "@/utils/types/subject";
-
-// const ScheduleSection: FC<{ schedule: ScheduleType }> = ({ schedule }) => {
+/**
+ * Displays the Student’s Schedule and relevant related information.
+ *
+ * @param schedule Data for displaying Schedule.
+ *
+ * @returns A Section.
+ */
 const ScheduleSection: FC<{ schedule: ScheduleType }> = ({ schedule }) => {
   const { t } = useTranslation("learn");
 
@@ -70,8 +68,9 @@ const ScheduleSection: FC<{ schedule: ScheduleType }> = ({ schedule }) => {
   );
 };
 
-// const SubjectListSection: FC<{ subjectList: SubjectListItem[] }> = ({
-const SubjectListSection: FC<{ subjectList: ClassroomSubject[] }> = ({ subjectList }) => {
+const SubjectListSection: FC<{
+  subjectList: ClassroomSubject[];
+}> = ({ subjectList }) => {
   const { t } = useTranslation("schedule");
   const locale = useLocale();
 
@@ -101,8 +100,7 @@ const SubjectListSection: FC<{ subjectList: ClassroomSubject[] }> = ({ subjectLi
 
 const LearnPage: CustomPage<{
   schedule: ScheduleType;
-  // subjectList: SubjectListItem[];
-  subjectList: any[];
+  subjectList: ClassroomSubject[];
 }> = ({ schedule, subjectList }) => {
   const { t } = useTranslation("learn");
 
