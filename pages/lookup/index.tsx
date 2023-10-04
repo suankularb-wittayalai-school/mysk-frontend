e219ee66b55afca598b6a37f530b7ada0a765c30
Siravit Phokeed <siravitsasa@gmail.com>
2023-10-04 22:15:30 +0700

    feat: new Lookup Landing

--- pages/lookup/index.tsx
+++ pages/lookup/index.tsx
@@ -1,16 +1,20 @@
 // Imports
 import PageHeader from "@/components/common/PageHeader";
-import cn from "@/utils/helpers/cn";
-import useLocale from "@/utils/helpers/useLocale";
+import LookupChoice from "@/components/lookup/landing/LookupChoice";
+import LookupLandingSubtitle from "@/components/lookup/landing/LookupLandingSubtitle";
 import { CustomPage, LangCode } from "@/utils/types/common";
-import { ContentLayout, Text } from "@suankularb-components/react";
+import {
+  Columns,
+  ContentLayout,
+  MaterialIcon,
+  Text,
+} from "@suankularb-components/react";
 import { GetStaticProps } from "next";
-import { useTranslation } from "next-i18next";
+import { Trans, useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 
 const LookupPage: CustomPage = () => {
-  const locale = useLocale();
   const { t } = useTranslation("lookup");
   const { t: tx } = useTranslation("common");
 
@@ -20,21 +24,40 @@ const LookupPage: CustomPage = () => {
         <title>{tx("tabName", { tabName: t("title") })}</title>
       </Head>
       <PageHeader>
-        <Text type="display-medium" className="block">
-          Lookup
+        <span className="md:hidden">{t("title")}</span>
+        <Text type="display-medium" className="hidden md:block">
+          {t("title")}
         </Text>
-        <Text type="headline-large" className="block">
-          <strong
-            className={cn(`bg-gradient-to-r from-primary to-secondary
-              bg-clip-text !font-bold [-webkit-text-fill-color:transparent]`)}
-          >
-            Find everything
-          </strong>{" "}
-          in MySK Lookup
-        </Text>
+        <LookupLandingSubtitle className="hidden md:block" />
       </PageHeader>
       <ContentLayout>
-        <Text type="body-medium">TODO</Text>
+        <LookupLandingSubtitle className="mx-4 sm:mx-0 sm:-mt-8 md:hidden" />
+        <Columns columns={2} className="mx-4 !gap-y-3 sm:mx-0">
+          <LookupChoice
+            icon={<MaterialIcon icon="face_6" size={48} />}
+            title={<Trans i18nKey="students.title" ns="lookup" />}
+            desc={t("students.desc")}
+            href="/lookup/students"
+          />
+          <LookupChoice
+            icon={<MaterialIcon icon="support_agent" size={48} />}
+            title={<Trans i18nKey="teachers.title" ns="lookup" />}
+            desc={t("teachers.desc")}
+            href="/lookup/teachers"
+          />
+          <LookupChoice
+            icon={<MaterialIcon icon="groups" size={48} />}
+            title={<Trans i18nKey="classes.title" ns="lookup" />}
+            desc={t("classes.desc")}
+            href="/lookup/classes"
+          />
+          <LookupChoice
+            icon={<MaterialIcon icon="document_scanner" size={48} />}
+            title={<Trans i18nKey="documents.title" ns="lookup" />}
+            desc={t("documents.desc")}
+            href="/lookup/documents"
+          />
+        </Columns>
       </ContentLayout>
     </>
   );
