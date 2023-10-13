be6a4813c725132b719ebbedb37a23d6b516d176
Siravit Phokeed <siravitsasa@gmail.com>
2023-10-13 20:31:01 +0700

    refactor: make comps from Lookup Details children

---
+++ components/lookup/LookupDetailsContent.tsx
@@ -0,0 +1,49 @@
+// Imports
+import cn from "@/utils/helpers/cn";
+import { StylableFC } from "@/utils/types/common";
+import { useAnimationConfig, transition } from "@suankularb-components/react";
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
+
+/**
+ * The second child of Lookup Details Card, which contains the rest of the
+ * content not in the header area.
+ * 
+ * ```txt
+ * +------------------------+
+ * | O Header               |
+ * |   [ Chip ] [ Chip ]    |
+ * |------------------------|
+ * | Content                | <-- Lookup Details Content
+ * |                        |
+ * |                        |
+ * |                        |
+ * |                        |
+ * +------------------------+
+ * ```
+ * 
+ * @param children The content of the Card.
+ */
+const LookupDetailsContent: StylableFC<{
+  children: ReactNode;
+}> = ({ children, style, className }) => {
+  const { duration, easing } = useAnimationConfig();
+
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={transition(duration.medium2, easing.standardDecelerate)}
+      style={style}
+      className={cn(
+        `flex grow flex-col gap-5 overflow-auto rounded-t-lg bg-surface-1 p-4
+        sm:overflow-visible md:overflow-auto`,
+        className,
+      )}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export default LookupDetailsContent;
