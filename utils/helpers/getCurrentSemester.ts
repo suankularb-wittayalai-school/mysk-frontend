aaffda2ece74ec3660da960fb5f0461d746e3c88
Siravit Phokeed <siravitsasa@gmail.com>
2023-10-15 15:12:22 +0700

    fix: getCurrentSemester always returns 1

--- utils/helpers/getCurrentSemester.ts
+++ utils/helpers/getCurrentSemester.ts
@@ -1,5 +1,7 @@
+/**
+ * Returns the current semester based on the current month.
+ */
 export default function getCurrentSemester(): 1 | 2 {
-  return 1;
   const month = new Date().getMonth() + 1;
   if (month >= 3 && month < 10) return 1;
   else return 2;
