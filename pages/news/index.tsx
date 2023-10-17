8bc4ba6478c5002d546e6f70776ac02623c622cf
Siravit Phokeed <siravitsasa@gmail.com>
2023-10-18 00:50:08 +0700

    refactor: getNewsFeed

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,20 +1,21 @@
 // Imports
 import PageHeader from "@/components/common/PageHeader";
-import NewsArticleItem from "@/components/news/NewsArticleItem";
 import LatestArticlesSection from "@/components/news/LatestArticlesSection";
-import mergeDBLocales from "@/utils/helpers/mergeDBLocales";
-import { DatabaseClient } from "@/utils/types/backend";
+import NewsArticleItem from "@/components/news/NewsArticleItem";
+import getNewsFeed from "@/utils/backend/news/getNewsFeed";
+import { supabase } from "@/utils/supabase-backend";
 import { CustomPage, LangCode } from "@/utils/types/common";
 import { NewsArticle } from "@/utils/types/news";
 import { Columns, ContentLayout } from "@suankularb-components/react";
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
-import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
+import { GetStaticProps } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 
 /**
  * A feed of News Articles, with the 2 most recent Articles highlighted.
+ * 
+ * @param newsFeed The News Articles to display.
  */
 const NewsPage: CustomPage<{ newsFeed: NewsArticle[] }> = ({ newsFeed }) => {
   const { t } = useTranslation("news");
@@ -45,39 +46,14 @@ const NewsPage: CustomPage<{ newsFeed: NewsArticle[] }> = ({ newsFeed }) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({
-  locale,
-  req,
-  res,
-}) => {
-  const supabase = createPagesServerClient({
-    req: req as NextApiRequest,
-    res: res as NextApiResponse,
-  }) as DatabaseClient;
-
-  const { data } = await supabase.from("infos").select("*, news(*)");
-
-  const newsFeed = data
-    ?.map((newsItem) => ({
-      id: newsItem.id,
-      title: mergeDBLocales(newsItem.news, "title"),
-      description: mergeDBLocales(newsItem.news, "description"),
-      image: newsItem.news!.image,
-      created_at: newsItem.news!.created_at,
-      old_url: newsItem.news!.old_url,
-      body: mergeDBLocales(newsItem, "body"),
-    }))
-    .sort((a, b) => {
-      if (a.created_at > b.created_at) return -1;
-      if (a.created_at < b.created_at) return 1;
-      return 0;
-    });
-
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  const { data: newsFeed } = await getNewsFeed(supabase);
   return {
     props: {
       ...(await serverSideTranslations(locale as LangCode, ["common", "news"])),
       newsFeed,
     },
+    revalidate: 300,
   };
 };
 
