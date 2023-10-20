294767c66d3a6526bcdc14e7910b63b3d5bdbe4c
Siravit Phokeed <siravitsasa@gmail.com>
2023-10-20 17:49:33 +0700

    refactor: remove unnecessary i18n config option
    localeDetection is already true by default

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 module.exports = {
   i18n: {
-    localeDetection: true,
     defaultLocale: "th",
     locales: ["th", "en-US"],
     localePath: path.resolve("./public/static/locales"),
