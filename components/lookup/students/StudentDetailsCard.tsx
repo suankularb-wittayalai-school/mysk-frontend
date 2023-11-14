8dd753b6b02d709d3626d79fe62e6677188c1f03
Siravit Phokeed <siravitsasa@gmail.com>
2023-11-14 13:38:13 +0700

    feat: translate Student Details Card

--- components/lookup/students/StudentDetailsCard.tsx
+++ components/lookup/students/StudentDetailsCard.tsx
@@ -1,17 +1,17 @@
 // Imports
 import MultilangText from "@/components/common/MultilingualText";
 import LookupDetailsCard from "@/components/lookup/LookupDetailsCard";
+import LookupDetailsContent from "@/components/lookup/LookupDetailsContent";
+import InformationCard from "@/components/lookup/people/InformationCard";
+import PersonContactGrid from "@/components/lookup/people/PersonContactGrid";
 import StudentHeader from "@/components/lookup/students/StudentHeader";
 import getLocaleName from "@/utils/helpers/getLocaleName";
-import useLocale from "@/utils/helpers/useLocale";
 import { StylableFC } from "@/utils/types/component";
 import { Student } from "@/utils/types/person";
+import { differenceInYears } from "date-fns";
 import { AnimatePresence } from "framer-motion";
 import { useTranslation } from "next-i18next";
 import { sift } from "radash";
-import LookupDetailsContent from "../LookupDetailsContent";
-import InformationCard from "../people/InformationCard";
-import PersonContactGrid from "../people/PersonContactGrid";
 
 /**
  * A Card that contains the details of a Student in Search Students.
@@ -21,7 +21,7 @@ import PersonContactGrid from "../people/PersonContactGrid";
 const StudentDetailsCard: StylableFC<{
   student?: Student;
 }> = ({ student, style, className }) => {
-  const locale = useLocale();
+  const { t } = useTranslation("lookup", { keyPrefix: "students.detail" });
   const { t: tx } = useTranslation("common");
 
   return (
@@ -32,7 +32,10 @@ const StudentDetailsCard: StylableFC<{
             <StudentHeader student={student} />
             <LookupDetailsContent>
               <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
-                <InformationCard title="Full name" className="col-span-2">
+                <InformationCard
+                  title={t("information.fullName")}
+                  className="col-span-2"
+                >
                   <MultilangText
                     text={{
                       th: getLocaleName("th", student, { prefix: true }),
@@ -48,7 +51,7 @@ const StudentDetailsCard: StylableFC<{
                 </InformationCard>
                 {student.nickname &&
                   sift(Object.values(student.nickname)).length > 0 && (
-                    <InformationCard title="Nickname">
+                    <InformationCard title={t("information.nickname")}>
                       <MultilangText
                         text={student.nickname}
                         options={{
@@ -59,23 +62,30 @@ const StudentDetailsCard: StylableFC<{
                     </InformationCard>
                   )}
                 {student.classroom && (
-                  <InformationCard title="Classroom">
-                    {[
-                      tx("class", { number: student.classroom.number }),
-                      `No. ${student.class_no}`,
-                    ].join(" • ")}
+                  <InformationCard title={t("information.classroom.title")}>
+                    {tx("class", { number: student.classroom.number })}
+                    <br />
+                    {t("information.classroom.classNo", {
+                      number: student.class_no,
+                    })}
                   </InformationCard>
                 )}
                 {student.birthdate &&
                   // Assuming no real person is born on Jan 1, 1970
                   student.birthdate !== "1970-01-01" && (
-                    <InformationCard title="Birthday">
+                    <InformationCard title={t("information.birthday.title")}>
                       <time>
-                        {new Date(student.birthdate).toLocaleDateString(
-                          locale,
-                          { day: "numeric", month: "long", year: undefined },
-                        )}
+                        {t("information.birthday.date", {
+                          date: new Date(student.birthdate),
+                        })}
                       </time>
+                      <br />
+                      {t("information.birthday.age", {
+                        count: differenceInYears(
+                          new Date(),
+                          new Date(student.birthdate),
+                        ),
+                      })}
                     </InformationCard>
                   )}
               </div>
