61baebb6a2706ec0ef29806205affedbf80a0cd1
Siravit Phokeed <siravitsasa@gmail.com>
2023-11-16 22:14:03 +0700

    fix: clamp Lookup Document Card subject

--- components/lookup/document/LookupDocumentCard.tsx
+++ components/lookup/document/LookupDocumentCard.tsx
@@ -2,12 +2,12 @@
 import cn from "@/utils/helpers/cn";
 import getLocaleYear from "@/utils/helpers/getLocaleYear";
 import useLocale from "@/utils/helpers/useLocale";
+import { StylableFC } from "@/utils/types/common";
 import { SchoolDocument } from "@/utils/types/news";
 import { Card, CardHeader } from "@suankularb-components/react";
 import { isThisYear } from "date-fns";
 import { useTranslation } from "next-i18next";
 import { camel } from "radash";
-import { FC } from "react";
 
 /**
  * A card that displays a Document in the list of Documents.
@@ -16,11 +16,11 @@ import { FC } from "react";
  * @param selected The currently selected Document.
  * @param onClick The function to set the selected Document.
  */
-const LookupDocumentCard: FC<{
+const LookupDocumentCard: StylableFC<{
   document: SchoolDocument;
   selected?: SchoolDocument;
   onClick: (value: SchoolDocument) => void;
-}> = ({ document, selected, onClick }) => {
+}> = ({ document, selected, onClick, style, className }) => {
   // Translation
   const locale = useLocale();
   const { t } = useTranslation("lookup", { keyPrefix: "documents.list" });
@@ -34,12 +34,14 @@ const LookupDocumentCard: FC<{
       direction="row"
       stateLayerEffect
       onClick={() => onClick(document)}
+      style={style}
       className={cn(
         `w-full items-center !rounded-none !border-transparent pr-3 text-left
         sm:!rounded-md`,
         selected?.id === document.id &&
           `sm:!border-outline-variant sm:!bg-primary-container
           sm:focus:!border-primary`,
+        className,
       )}
     >
       {/* Subject line, code, and signed date */}
@@ -60,6 +62,7 @@ const LookupDocumentCard: FC<{
             day: "numeric",
           }),
         })}
+        className="[&_h3]:line-clamp-3"
       />
     </Card>
   );
