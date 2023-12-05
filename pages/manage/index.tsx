0d086d5571f059d6cafe94d645881e4d1f4bc561
Siravit Phokeed <siravitsasa@gmail.com>
2023-12-05 12:56:51 +0700

    feat: Manage page Sections

--- pages/manage/index.tsx
+++ pages/manage/index.tsx
@@ -1,13 +1,23 @@
 // Imports
+import PageHeader from "@/components/common/PageHeader";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import getUserByEmail from "@/utils/backend/account/getUserByEmail";
 import permitted from "@/utils/helpers/permitted";
 import { CustomPage, LangCode } from "@/utils/types/common";
 import { UserPermissionKey, UserRole } from "@/utils/types/person";
+import {
+  Columns,
+  ContentLayout,
+  Header,
+  Section,
+  Text,
+} from "@suankularb-components/react";
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
 
 /**
  * Management’s counterpart to Learn, where the user can view statistics about
@@ -16,10 +26,42 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
  *
  * Management users see this page as their Home. Other users with the permission
  * can access this page from their Account page.
- *
- * @todo Implement this page.
  */
-const ManagePage: CustomPage = () => null;
+const ManagePage: CustomPage = () => {
+  const { t } = useTranslation("manage");
+  const { t: tx } = useTranslation("common");
+
+  return (
+    <>
+      <Head>
+        <title>{tx("tabName", { tabName: "Manage" })}</title>
+      </Head>
+      <PageHeader>{tx("appName")}</PageHeader>
+      <ContentLayout>
+        {/* Attendance */}
+        <Section>
+          <Header>Attendance</Header>
+          <Text type="body-medium" className="!-mt-2">
+            Every morning, teachers take attendance of their students during
+            assembly and homeroom. Here’s a summary.
+          </Text>
+          <Columns columns={2}>{}</Columns>
+        </Section>
+
+        {/* Participation */}
+        <Columns columns={6}>
+          <Section className="col-span-2 sm:col-span-4 md:col-start-2">
+            <Header>Participation</Header>
+            <Text type="body-medium" className="!-mt-2">
+              Metrics on students and teachers participation in using the MySK
+              infrastructure.
+            </Text>
+          </Section>
+        </Columns>
+      </ContentLayout>
+    </>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async ({
   locale,
@@ -40,9 +82,11 @@ export const getServerSideProps: GetServerSideProps = async ({
   if (
     !(
       // The user is a management user.
-      user?.role === UserRole.management ||
-      // The user has the `can_see_management` permission.
-      permitted(user!, UserPermissionKey.can_see_management)
+      (
+        user?.role === UserRole.management ||
+        // The user has the `can_see_management` permission.
+        permitted(user!, UserPermissionKey.can_see_management)
+      )
     )
   )
     return { notFound: true };
