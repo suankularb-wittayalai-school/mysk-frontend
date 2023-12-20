a8a8fdd82e44f4843b167d3d1d1f4627a84bca7f
Siravit Phokeed <siravitsasa@gmail.com>
2023-12-20 21:04:25 +0700

    docs: Curr. Learning Period Card refers to Teacher
    It should refer to Classroom instead.

--- components/lookup/students/CurrentLearningPeriodCard.tsx
+++ components/lookup/students/CurrentLearningPeriodCard.tsx
@@ -6,9 +6,9 @@ import { UserRole } from "@/utils/types/person";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 /**
- * An implementation of Current Period Card for Search Teachers.
+ * An implementation of Current Period Card for a Classroom.
  *
- * @param teacherID The ID of the Teacher to get the current period of.
+ * @param teacherID The ID of the Classroom to get the current period of.
  * @param onClick Triggers when the Card is clicked. Should open the Schedule.
  */
 const CurrentLearningPeriodCard: StylableFC<{
