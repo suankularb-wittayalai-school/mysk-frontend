c5b2dd02715347377c1ac044fe6b149b903a0b54
Siravit Phokeed <siravitsasa@gmail.com>
2024-01-21 11:43:55 +0700

    feat: selected Profile Tab

--- components/account/ProfileNavigation.tsx
+++ components/account/ProfileNavigation.tsx
@@ -6,29 +6,27 @@ import { MaterialIcon } from "@suankularb-components/react";
 /**
  * A set of tabs for navigating between Profile pages.
  */
-const ProfileNavigation: StylableFC = ({ style, className }) => {
-  return (
-    <nav style={style} className={cn(`space-y-1`, className)}>
-      <ProfileTab
-        icon={<MaterialIcon icon="account_circle" />}
-        href="/account/about"
-      >
-        About you
-      </ProfileTab>
-      <ProfileTab
-        icon={<MaterialIcon icon="contacts" />}
-        href="/account/contacts"
-      >
-        Your contacts
-      </ProfileTab>
-      <ProfileTab
-        icon={<MaterialIcon icon="developer_guide" />}
-        href="/account/certificates"
-      >
-        Certificates
-      </ProfileTab>
-    </nav>
-  );
-};
+const ProfileNavigation: StylableFC = ({ style, className }) => (
+  <nav style={style} className={cn(`space-y-1`, className)}>
+    <ProfileTab
+      icon={<MaterialIcon icon="account_circle" />}
+      href="/account/about"
+    >
+      About you
+    </ProfileTab>
+    <ProfileTab
+      icon={<MaterialIcon icon="contacts" />}
+      href="/account/contacts"
+    >
+      Your contacts
+    </ProfileTab>
+    <ProfileTab
+      icon={<MaterialIcon icon="developer_guide" />}
+      href="/account/certificates"
+    >
+      Certificates
+    </ProfileTab>
+  </nav>
+);
 
 export default ProfileNavigation;
--- components/account/ProfileTab.tsx
+++ components/account/ProfileTab.tsx
@@ -2,14 +2,24 @@ import cn from "@/utils/helpers/cn";
 import { StylableFC } from "@/utils/types/common";
 import { Interactive, Text } from "@suankularb-components/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
 
+/**
+ * A tab in Profile Navigation.
+ *
+ * @param children The text of the tab.
+ * @param icon The icon of the tab.
+ * @param href The URL the tab goes to. Also used in determining if this tab is selected.
+ */
 const ProfileTab: StylableFC<{
   children: ReactNode;
   icon: JSX.Element;
   href: string;
-  selected?: boolean;
 }> = ({ children, icon, href, style, className }) => {
+  const { pathname } = useRouter();
+  const selected = pathname.startsWith(href);
+
   return (
     <Interactive
       href={href}
@@ -17,11 +27,22 @@ const ProfileTab: StylableFC<{
       style={style}
       className={cn(
         `grid grid-cols-[1.5rem,1fr] gap-3 rounded-full px-3 py-2`,
+        selected && `bg-primary-container text-on-primary-container`,
         className,
       )}
     >
-      <div className="text-on-surface-variant">{icon}</div>
-      <Text type="title-medium">{children}</Text>
+      <div
+        className={
+          selected
+            ? `[&>.skc-icon]:[font-variation-settings:'FILL'1]` // Fill icon
+            : `text-on-surface-variant`
+        }
+      >
+        {icon}
+      </div>
+      <Text type="title-medium" className="truncate">
+        {children}
+      </Text>
     </Interactive>
   );
 };
