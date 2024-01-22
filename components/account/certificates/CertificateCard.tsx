c5bf12af63a8d811e4e77b22cf652481bc5c6a1f
Siravit Phokeed <siravitsasa@gmail.com>
2024-01-22 16:32:11 +0700

    feat: Certificate Cards

---
+++ components/account/certificates/CertificateCard.tsx
@@ -0,0 +1,72 @@
+import MultiSchemeImage from "@/components/common/MultiSchemeImage";
+import BlobsLeftDark from "@/public/images/graphics/blobs/left-dark.svg";
+import BlobsLeftLight from "@/public/images/graphics/blobs/left-light.svg";
+import cn from "@/utils/helpers/cn";
+import { StudentCertificate } from "@/utils/types/certificate";
+import { StylableFC } from "@/utils/types/common";
+import { Card, MaterialIcon, Text } from "@suankularb-components/react";
+import { useTranslation } from "next-i18next";
+import { camel } from "radash";
+
+/**
+ * A Card that displays a Student Certificate.
+ *
+ * @param certificate The Student Certificate to display.
+ */
+const CertificateCard: StylableFC<{
+  certificate: StudentCertificate;
+}> = ({ certificate, style, className }) => {
+  const { t } = useTranslation("account", { keyPrefix: "certificates" });
+
+  return (
+    <Card
+      appearance="outlined"
+      style={style}
+      className={cn(
+        `relative isolate !grid grid-cols-[3.375rem,1fr] !gap-4 overflow-hidden
+        !rounded-lg p-4`,
+        className,
+      )}
+    >
+      <MultiSchemeImage
+        srcLight={BlobsLeftLight}
+        srcDark={BlobsLeftDark}
+        alt=""
+        priority
+        className="absolute inset-0 -z-10 [&_img]:h-full [&_img]:object-cover"
+      />
+
+      {/* Icon */}
+      <div className="relative aspect-square">
+        <MaterialIcon icon="developer_guide" size={48} />
+        <div
+          className={cn(`absolute bottom-0 right-0 z-10 aspect-square w-fit
+            rounded-full bg-secondary p-1 text-on-secondary`)}
+        >
+          {
+            {
+              student_of_the_year: <MaterialIcon icon="award_star" />,
+              excellent_student: <MaterialIcon icon="star" />,
+              academic: <MaterialIcon icon="school" />,
+              morale: <MaterialIcon icon="folded_hands" />,
+              sports: <MaterialIcon icon="sports_football" />,
+              activity: <MaterialIcon icon="category" />,
+            }[certificate.certificate_type]
+          }
+        </div>
+      </div>
+
+      {/* Text */}
+      <div className="sm:col-span-2 md:col-span-1">
+        <Text type="headline-small" element="h3">
+          {t(`type.${camel(certificate.certificate_type)}`)}
+        </Text>
+        <Text type="title-medium" element="p" className="empty:hidden">
+          {certificate.certificate_detail}
+        </Text>
+      </div>
+    </Card>
+  );
+};
+
+export default CertificateCard;
