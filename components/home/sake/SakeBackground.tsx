f3b34b7a49795b42971267784b0413bc0a9b3d88
Siravit Phokeed <siravitsasa@gmail.com>
2024-02-23 13:54:55 +0700

    refactor: Decoration Blobs in Sake Background

--- components/home/sake/SakeBackground.tsx
+++ components/home/sake/SakeBackground.tsx
@@ -1,63 +1,39 @@
+import BlobsBlurringGroup from "@/components/common/BlobsBlurringGroup";
+import DecorativeBlob from "@/components/common/DecorativeBlob";
+import MySKLogo from "@/public/images/brand/mysk-light.svg";
+import cn from "@/utils/helpers/cn";
 import { StylableFC } from "@/utils/types/common";
+import Image from "next/image";
 
 /**
  * The background for Sake Celebration.
  */
 const SakeBackground: StylableFC = ({ style, className }) => {
   return (
-    <svg
-      width={560}
-      height={256}
-      viewBox="0 0 560 256"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      style={style}
-      className={className}
+    <div
+      style={{ width: 560, height: 256, ...style }}
+      className={cn(`*:absolute`, className)}
     >
       {/* Blobs */}
-      <g filter="url(#filter-sake)">
-        <path
-          d="M607.336 236.441C607.336 287.562 425.871 383 374.749 383C323.628 383 260.222 277.819 260.222 226.697C260.222 175.576 432.796 34.0593 483.917 34.0593C535.039 34.0593 607.336 185.319 607.336 236.441Z"
-          className="fill-secondary-90 dark:fill-secondary-10"
+      <BlobsBlurringGroup className="inset-0 *:absolute">
+        <DecorativeBlob
+          variant="secondary-1"
+          style={{ width: 347, height: 348, top: 34, left: 260 }}
         />
-        <path
-          d="M692.167 63.29C692.167 114.411 409.978 133.322 358.856 133.322C307.735 133.322 260 44.1768 260 -6.94472C260 -58.0662 353.408 -65 404.529 -65C455.651 -65 692.167 12.1685 692.167 63.29Z"
-          className="fill-primary-90 dark:fill-primary-10"
+        <DecorativeBlob
+          variant="primary-2"
+          style={{ width: 432, height: 198, top: -65, left: 260 }}
         />
-      </g>
+      </BlobsBlurringGroup>
 
       {/* Logo */}
-      <image
-        href="/images/brand/mysk-light.svg"
-        x={275}
-        y={24}
-        width={384}
-        height={384}
-        opacity={0.1}
+      <Image
+        src={MySKLogo}
+        style={{ width: 384, height: 384, top: 24, left: 275 }}
+        alt=""
+        className="opacity-10"
       />
-
-      {/* Blur filter */}
-      <defs>
-        <filter
-          id="filter-sake"
-          x={160}
-          y={-165}
-          width={632}
-          height={648}
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          />
-          <feGaussianBlur stdDeviation={50} />
-        </filter>
-      </defs>
-    </svg>
+    </div>
   );
 };
 
