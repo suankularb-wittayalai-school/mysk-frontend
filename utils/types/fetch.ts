32916f79040512bd1d02c891efcfa66c991bf482
Siravit Phokeed <siravitsasa@gmail.com>
2024-04-10 01:24:11 +0700

    docs: API types

--- utils/types/fetch.ts
+++ utils/types/fetch.ts
@@ -1,20 +1,40 @@
+/**
+ * The level of detail for fetching data.
+ * 
+ * - `id_only`: Only the ID.
+ * - `compact`: Compact representation for lists.
+ * - `default`: Most details needed.
+ * - `detailed`: Includes all details and relationships.
+ */
 export type FetchLevel = "id_only" | "compact" | "default" | "detailed";
 
+/**
+ * Configuration for filtering data.
+ */
 export type FilterConfig<Data extends {} | unknown = unknown> = Partial<{
   data: Data;
   q: string;
 }>;
 
+/**
+ * Configuration for sorting data.
+ */
 export type SortingConfig<Data extends {} | unknown = unknown> = {
   by: Data[];
   ascending?: boolean;
 };
 
+/**
+ * Configuration for paginating data.
+ */
 export type PaginationConfig = {
   p: number;
   size?: number;
 };
 
+/**
+ * Queries for fetching data.
+ */
 export type Query<Data extends {} | unknown = unknown> = {
   pagination?: PaginationConfig;
   filter?: FilterConfig<Data>;
@@ -23,6 +43,21 @@ export type Query<Data extends {} | unknown = unknown> = {
   descendant_fetch_level?: FetchLevel;
 };
 
+/**
+ * The response data from `/auth/oauth/google`, used to set authentication
+ * cookies.
+ */
+export type OAuthResponseData = {
+  access_token: string;
+  expires_in: number;
+  id_token: string;
+  scope: string;
+  token_type: string;
+};
+
+/**
+ * An error object from a fetch request.
+ */
 export type FetchError = {
   code: number;
   error_type: string;
@@ -30,6 +65,9 @@ export type FetchError = {
   source: string;
 };
 
+/**
+ * A response from a fetch request.
+ */
 export type FetchReturn<Data extends {} | unknown = unknown> = {
   api_version: string;
   meta: { timestamp: string; pagination: null } | null;
