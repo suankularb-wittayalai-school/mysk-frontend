57edb57f67f95d13d21e445f420461f5bd751dab
Siravit Phokeed <siravitsasa@gmail.com>
2024-04-10 02:05:05 +0700

    fix: oops
    Let’s not talk about this.

--- utils/helpers/account/useOneTapSignin.ts
+++ utils/helpers/account/useOneTapSignin.ts
@@ -71,7 +71,7 @@ export default function useOneTapSignin(
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ credential: credential + "penis" }),
+        body: JSON.stringify({ credential }),
       },
     );
     if (error) {
