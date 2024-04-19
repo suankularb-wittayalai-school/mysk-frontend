21965b1022fbcfa6adf20a9a03acb56ee32fbbdc
Siravit Phokeed <siravitsasa@gmail.com>
2024-04-20 01:04:40 +0700

    docs: getLocaleYear JSDoc

--- utils/helpers/getLocaleYear.ts
+++ utils/helpers/getLocaleYear.ts
@@ -2,6 +2,15 @@ import { LangCode } from "@/utils/types/common";
 
 const BE_AD_DIFFERENCE = 543;
 
+/**
+ * Converts a year between the Buddhist Era (BE) and the Anno Domini (AD) calendar.
+ *
+ * @param locale The locale to convert the year to.
+ * @param year The year to convert.
+ * @param fromType The type of the year to convert from, either "AD" or "BE".
+ *
+ * @returns The converted year.
+ */
 export default function getLocaleYear(
   locale: LangCode,
   year: number,
