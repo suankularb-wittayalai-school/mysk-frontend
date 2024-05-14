91cc6857cc78639d0599d36f030e2a6cf0d6141a
Siravit Phokeed <siravitsasa@gmail.com>
2024-05-15 03:15:34 +0700

    feat: translate Admin Panel page

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,7 +1,6 @@
 import PageHeader from "@/components/common/PageHeader";
 import ManagePageCard from "@/components/manage/ManagePageCard";
 import cn from "@/utils/helpers/cn";
-import useLocale from "@/utils/helpers/useLocale";
 import { CustomPage, LangCode } from "@/utils/types/common";
 import {
   Button,
@@ -19,14 +18,10 @@ import Link from "next/link";
 import { forwardRef } from "react";
 
 /**
- * A landing page for admins. Displays statistics (including the number of
- * students, teachers, classes, news articles, and other information related to
- * those, grouped into Cards) and a link to Supabase.
- *
- * @param count A number of statistics passed in from `getStaticProps`.
+ * A landing page for admins. Contains links to manage the database and the
+ * schedule editor.
  */
 const AdminPanelPage: CustomPage = () => {
-  const locale = useLocale();
   const { t } = useTranslation("admin");
   const { t: tx } = useTranslation("common");
 
@@ -44,23 +39,24 @@ const AdminPanelPage: CustomPage = () => {
       <Head>
         <title>{tx("tabName", { tabName: t("title") })}</title>
       </Head>
-      <PageHeader parentURL="/account">{t("title")}</PageHeader>
+      <PageHeader>{t("title")}</PageHeader>
       <ContentLayout>
         <Columns columns={2} className="mx-4 sm:mx-0">
           <ManagePageCard
             icon={<MaterialIcon icon="bolt" size={48} />}
-            title="Supabase"
-            desc="The MySK database is stored in Supabase and fetched via Render. View and edit tables in its dashboard."
+            title={t("supabase.title")}
+            desc={t("supabase.desc")}
           >
             <Button
               appearance="filled"
               href={supabaseURL}
+              onClick={() => plausible("Open Supabase Table Editor")}
               // eslint-disable-next-line react/display-name
               element={forwardRef((props, ref) => (
                 <a {...props} ref={ref} target="_blank" />
               ))}
             >
-              Browse
+              {t("supabase.action.browse")}
             </Button>
             <Button
               appearance="outlined"
@@ -70,16 +66,16 @@ const AdminPanelPage: CustomPage = () => {
                 <a {...props} ref={ref} target="_blank" />
               ))}
             >
-              Read docs
+              {t("supabase.action.readDocs")}
             </Button>
           </ManagePageCard>
           <ManagePageCard
             icon={<MaterialIcon icon="dashboard" size={48} />}
-            title="Schedule"
-            desc="Correct any schedule import errors of any classroom using tools meant for teachers."
+            title={t("schedule.title")}
+            desc={t("schedule.desc")}
           >
             <Button appearance="filled" href="/admin/schedule" element={Link}>
-              Edit
+              {t("schedule.action.continue")}
             </Button>
           </ManagePageCard>
         </Columns>
@@ -90,10 +86,7 @@ const AdminPanelPage: CustomPage = () => {
             md:w-[calc(50%-3rem)]`)}
         >
           <MaterialIcon icon="warning" />
-          <Text type="title-medium">
-            Any changes made in this panel will immediately affect users.
-            Proceed with caution.
-          </Text>
+          <Text type="title-medium">{t("warning")}</Text>
         </div>
       </ContentLayout>
     </>
