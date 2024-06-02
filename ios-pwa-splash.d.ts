172cd7c634764f208778c8e0dcb3f6b2d637c04c
Siravit Phokeed <siravitsasa@gmail.com>
2024-06-02 18:04:54 +0700

    feat: ios-pwa-splash

---
+++ ios-pwa-splash.d.ts
@@ -0,0 +1,7 @@
+declare module "ios-pwa-splash" {
+  /** @see https://github.com/avadhesh18/iosPWASplash */
+  export default function iosPWASplash(
+    icon: string,
+    backgroundColor: string = "white",
+  ): void;
+}
