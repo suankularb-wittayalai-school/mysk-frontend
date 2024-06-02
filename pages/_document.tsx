dc7b9039f853d26363234e7c0b2ba2c99de5334b
Siravit Phokeed <siravitsasa@gmail.com>
2024-06-02 16:13:54 +0700

    feat: update icons to use new colors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,28 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+const APP_NAME = "MySK";
+
 export default function Document() {
   return (
     <Html>
       <Head>
-        {/* Icons */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/icons/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+        <link rel="icon" type="image/png" href="/icons/favicon.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#b50077" />
-        <meta name="msapplication-TileColor" content="#2b5797" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#a43560"
+        />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="application-name" content={APP_NAME} />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           key="theme-color-light"
