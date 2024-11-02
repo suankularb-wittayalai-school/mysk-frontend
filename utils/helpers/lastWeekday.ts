d310f965f3cfffacefba2ae93ba237f612985772
PixelEdition <pixel.pxed@gmail.com>
2024-11-02 07:25:27 +0700

    feat: check with environment variable

--- utils/helpers/lastWeekday.ts
+++ utils/helpers/lastWeekday.ts
@@ -10,9 +10,13 @@ import { addDays, getDate, setDay } from "date-fns";
 export default function lastWeekday(date: Date) {
   const day = date.getDay();
 
-  // To-do: Fix this hard code (blame @pixelpxed)
-  if ((`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`) == '2024-11-2') return date
-  
+  const getDate = date.getDate();
+  const getMonth = date.getMonth() + 1;
+  if (
+    `${date.getFullYear()}-${getMonth + 1 > 9 ? getMonth : "0" + getMonth}-${getDate + 1 > 9 ? getDate : "0" + getDate}` ==
+    process.env.ATTENDANCE_SPECIAL_DATE
+  ) return date;
+
   if (day === 0) return setDay(addDays(date, -1), 5);
   if (day === 6) return setDay(date, 5);
   return date;
