545d23a30196b2749bbbfc61cccda4af7b1f2e8c
Sun-52 <iamsun2552@gmail.com>
2025-01-27 16:56:37 +0700

    fix: parameter mispell

--- components/report/ReportInputForm.tsx
+++ components/report/ReportInputForm.tsx
@@ -51,7 +51,7 @@ const ReportInputForm: FC<{
     report.length > 0 ? report[0].suggestions : null,
   );
   const [teachingMethod, setTeachingMethod] = useState<String>(
-    report.length > 0 ? report[0].teaching_method[0] : "Live Course",
+    report.length > 0 ? report[0].teaching_methods[0] : "Live Course",
   );
   const locale = useLocale();
   const mysk = useMySKClient();
