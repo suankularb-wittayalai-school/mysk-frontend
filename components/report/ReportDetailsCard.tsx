bf4c6224ad78088c192c5469cbc026087ddc8112
Sun-52 <iamsun2552@gmail.com>
2025-01-27 08:23:51 +0700

    feat: create report function

--- components/report/ReportDetailsCard.tsx
+++ components/report/ReportDetailsCard.tsx
@@ -5,12 +5,12 @@ import ReportInputForm from "@/components/report/ReportInputForm";
 import { StylableFC } from "@/utils/types/common";
 import { Teacher } from "@/utils/types/person";
 import { Header } from "@suankularb-components/react";
+import { MySKClient } from "@/utils/types/fetch";
+import { SupabaseClient } from "@supabase/supabase-js";
 
-const ReportDetailsCard: StylableFC<{ teacher: Teacher }> = ({
-  teacher,
-  style,
-  className,
-}) => {
+const ReportDetailsCard: StylableFC<{
+  teacher: Teacher;
+}> = ({ teacher, style, className }) => {
   console.log(teacher.subjects_in_charge[0].name, "where are you");
   return (
     <LookupDetailsCard>
