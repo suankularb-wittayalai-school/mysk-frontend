b138e4a02ae46cc0484c9d93485ef746c5427ee8
Sun-52 <iamsun2552@gmail.com>
2025-10-30 18:48:04 +0700

    fix: student cheer attendance page period display

--- components/cheer/StudentCheerAttendanceListItem.tsx
+++ components/cheer/StudentCheerAttendanceListItem.tsx
@@ -39,13 +39,19 @@ const StudentCheerAttendanceListItem: FC<{
             title={t("date", {
               date: new Date(attendance.practice_period.date),
             })}
-            desc={t("period", {
-              start: attendance.practice_period.start_time,
-              end:
-                attendance.practice_period.start_time +
-                attendance.practice_period.duration -
-                1,
-            })}
+            desc={
+              attendance.practice_period.duration != 1
+                ? t("period.multiple", {
+                    start: attendance.practice_period.start_time,
+                    end:
+                      attendance.practice_period.start_time +
+                      attendance.practice_period.duration -
+                      1,
+                  })
+                : t("period.single", {
+                    start: attendance.practice_period.start_time,
+                  })
+            }
             className="w-0 [&>span]:!truncate"
           />
           <CheerAttendanceSelector
