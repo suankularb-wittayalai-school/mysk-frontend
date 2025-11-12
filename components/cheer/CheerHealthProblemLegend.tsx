f3ce70b62efb25bc2ae25a816b732f4da7650cbe
Nubwan <149947073+Nubboyy@users.noreply.github.com>
2025-11-12 21:53:36 +0700

    style: allow text wrap

--- components/cheer/CheerHealthProblemLegend.tsx
+++ components/cheer/CheerHealthProblemLegend.tsx
@@ -10,7 +10,7 @@ const CheerHealthProblemLegend: StylableFC = ({ style, className }) => {
     <ul
       style={style}
       className={cn(
-        `mx-4 flex flex-row flex-wrap justify-center gap-x-3 gap-y-1 rounded-md border-1 border-outline-variant p-2 pr-3 *:flex *:flex-row *:items-center *:gap-2 sm:mx-auto md:border-0 md:p-0 [&_div]:!w-5 [&_span]:whitespace-nowrap [&_span]:text-on-surface-variant`,
+        `mx-4 flex flex-row flex-wrap justify-center gap-x-3 gap-y-1 rounded-md border-1 border-outline-variant p-2 pr-3 *:flex *:flex-row *:items-center *:gap-2 sm:mx-auto md:border-0 md:p-0 [&_div]:!w-5 [&_span]:text-on-surface-variant`,
         className,
       )}
     >
