7d5fe29401f5b2b2447d6f919ac8af8401c110c6
Sun-52 <iamsun2552@gmail.com>
2025-11-14 19:00:39 +0700

    feat: show allergies and health problems in StudentDetailsCard

--- utils/backend/person/getStudentsByLookupFilters.ts
+++ utils/backend/person/getStudentsByLookupFilters.ts
@@ -37,9 +37,11 @@ export default async function getStudentsByLookupFilters(
         last_name_en,
         nickname_en,
         profile,
+        person_allergies(allergy_name),
         person_contacts${filters.contact ? "!inner" : ""}(contacts!inner(value))
       ),
-      classroom_students(classrooms!inner(id, number))`,
+      classroom_students(classrooms!inner(id, number)),
+      cheer_practice_medical_risk_students(condition)`,
     )
     .eq("classroom_students.classrooms.year", getCurrentAcademicYear());
 
@@ -78,6 +80,13 @@ export default async function getStudentsByLookupFilters(
       profile: student.people?.profile || null,
       role: UserRole.student,
       classroom: student.classroom_students[0]?.classrooms || null,
+      allergies: student!.people.person_allergies.map(
+        ({ allergy_name }) => allergy_name,
+      ),
+      health_problem:
+        student!.cheer_practice_medical_risk_students.map(
+          ({ condition }) => condition,
+        )[0] || "",
     })),
     error: null,
   };
