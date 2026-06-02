c053aa59391f90423f61349f8154463b26776c99
Sun-52 <iamsun2552@gmail.com>
2026-06-02 20:13:56 +0700

    fix: build error

--- pages/organization.tsx
+++ pages/organization.tsx
@@ -1,9 +1,8 @@
 import useMySKClient from "@/utils/backend/mysk/useMySKClient";
-import { CustomPage } from "@/utils/types/common";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-const OrganizationPage: CustomPage = () => {
+const OrganizationPage = () => {
   const mysk = useMySKClient();
   const router = useRouter();
   useEffect(() => {
